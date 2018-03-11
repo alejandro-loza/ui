8bfa7af1fbeb13b3f7b87ecc2142f61506fbd657
Aarón Cervantes <aaron.cervantes@dinerio.mx>
2018-03-10 21:25:21 -0600

    Good practices for routes

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -6,7 +6,7 @@ import { SignupComponent } from './login/signup/signup.component';
 const APP_ROUTING: Routes = [
 	{path: 'login', component: LoginComponent},
 	{path: 'register', component: SignupComponent},	
-	{path: '**', component: LoginComponent},
+	{path: '**', component: LoginComponent}
 ];
 
 export const APP_ROUTES = RouterModule.forRoot(APP_ROUTING);
