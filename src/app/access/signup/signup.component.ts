84146d7d4e44a1510b2ae294b61fbf6c790e30a1
Aarón Cervantes <aaron.cervantes@finerio.mx>
2018-03-16 02:04:44 -0600

    Designed signup component, added style and hover for social_media

---
+++ src/app/access/signup/signup.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  	this.hoverSocialMedia();
+  }
+
+  hoverSocialMedia(){
+    
+    var btn_facebook = document.querySelector('.btn-facebook');
+    var btn_google = document.querySelector('.btn-google-plus');
+
+    btn_facebook.addEventListener("mouseover", this.overFacebook);
+    btn_facebook.addEventListener("mouseout", this.outFacebook);
+
+    btn_google.addEventListener("mouseover", this.overGoogle);
+    btn_google.addEventListener("mouseout", this.outGoogle);
+  }
+
+  overFacebook(){
+    var btn_facebook_blue = document.querySelector('.icon-facebook-blue');
+    var btn_facebook_white = document.querySelector('.icon-facebook-white');
+
+    btn_facebook_white.classList.add("d-none");
+    btn_facebook_blue.classList.remove("d-none");
+  }
+
+  outFacebook(){
+    let btn_facebook_blue = document.querySelector('.icon-facebook-blue');
+    let btn_facebook_white = document.querySelector('.icon-facebook-white');
+
+    btn_facebook_blue.classList.add('d-none');
+    btn_facebook_white.classList.remove('d-none');
+  }
+
+  overGoogle(){
+    let btn_google_red = document.querySelector('.icon-google-plus-red');
+    let btn_google_white = document.querySelector('.icon-google-plus-white');
+
+    btn_google_white.classList.add('d-none');
+    btn_google_red.classList.remove('d-none'); 
+  }
+
+  outGoogle(){
+    let btn_google_red = document.querySelector('.icon-google-plus-red');
+    let btn_google_white = document.querySelector('.icon-google-plus-white');
+
+    btn_google_white.classList.remove('d-none');
+    btn_google_red.classList.add('d-none');
+  }
+
+}
