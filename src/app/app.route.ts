92d5a9d66820cfb66d38e1d4436eea895738fe95
Aarón Cervantes <aaron.cervantes@finerio.mx>
2018-03-16 02:02:19 -0600

    Added new route, changed register to signup

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,12 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './login/login/login.component';
-import { SignupComponent } from './login/signup/signup.component';
+import { LoginComponent } from './access/login/login.component';
+import { SignupComponent } from './access/signup/signup.component';
 
 const APP_ROUTING: Routes = [
 	{path: 'login', component: LoginComponent},
-	{path: 'register', component: SignupComponent},	
-	{path: '**', component: LoginComponent}
+    {path: 'signup', component: SignupComponent},
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
+    {path: '**', redirectTo: '/login'}
 ];
 
 export const APP_ROUTES = RouterModule.forRoot(APP_ROUTING);
