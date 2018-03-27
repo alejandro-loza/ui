4da530f50a2cd259a4a1f47da555a7d8c5f3719a
Aarón Cervantes <aaron.cervantes@dinerio.mx>
2018-03-26 18:07:55 -0600

    Added pages.route

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PAGES_ROUTES } from './pages.route'
+
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages.component'
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
   declarations: [
+  	PagesComponent,
   	DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    PAGES_ROUTES
   ]
 })
 export class PagesModule { }
