4a061fb088ac642ad4334bf40aaf0191faf3b5e2
Aarón Cervantes <aaron.cervantes@dinerio.mx>
2018-03-27 02:11:14 -0600

    Added just space

--- src/app/pages/pages.route.ts
+++ src/app/pages/pages.route.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import { AuthGuard } from './../guards/auth/auth.guard'
+
 const PAGES_ROUTING: Routes = [
 	{
 		path:'',
