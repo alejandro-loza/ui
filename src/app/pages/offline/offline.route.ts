dec46a26dd65c603ab84dac61f67b4d0a79c091a
AlanVHz <alan.vargas@finerio.mx>
2018-04-05 17:04:45 -0500

    offline removed

--- src/app/pages/offline/offline.route.ts
+++
@@ -1,8 +0,0 @@
-import { RouterModule, Routes } from '@angular/router';
-import { OfflineComponent } from './components/offline.component';
-
-const OFFLINE_ROUTING: Routes = [
-    { path: '', component: OfflineComponent }
-];
-
-export const OFFLINE_ROUTES = RouterModule.forChild( OFFLINE_ROUTING );
