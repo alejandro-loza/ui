b607ecd4aca0dce9d98b26af48b865c719854f6b
Aarón Cervantes <aaron.cervantes@finerio.mx>
2018-04-16 10:56:09 -0500

    Created new function to save and get Data, modified login function, and added config.service

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,32 +1,55 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { environment } from './../../../environments/environment';
-import { User } from './../../shared/dto/authLoginDot';
+import { FinerioService } from '../shared/config.service';
 
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-}
+import { User } from './../../shared/dto/authLoginDot';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
-  private backendUrl: string = `${environment.backendUrl}api`;
-  url:string;
+
   user: User;
-  constructor( private httpClient: HttpClient) {  
+  token: string;
+
+  private backendUrl = `${environment.backendUrl}/api`;
+  constructor(
+    private httpClient: HttpClient,
+    private finerioService: FinerioService) {
+      this.getData();
+    }
+
+  isAuth() {
+    return (  this.token.length  > 0 ) ? true : false;
   }
 
-  isAuth():boolean {
-    return false;
+  getData() {
+    if ( localStorage.getItem('access token') ) {
+      this.token = localStorage.getItem('access token');
+    } else {
+      this.token = '';
+    }
+  }
+
+  saveData( access_token: string, refresh_token: string, username: string ) {
+    localStorage.setItem( 'access token', access_token );
+    localStorage.setItem( 'refresh token', refresh_token );
+    localStorage.setItem( 'username', username );
+
+    this.finerioService.setToken( refresh_token );
   }
 
   login(user: User){
-  	let url = `${this.backendUrl}/login`;
-    return this.httpClient.post<User>(
-        url,
-        JSON.stringify({username: user.username, password: user.password}),
-        httpOptions).subscribe(res => { console.log(res)});
-    
+    const url = `${this.backendUrl}/login`;
+    return this.httpClient.post(
+      url, JSON.stringify({ username: user.email, password: user.password }), {headers : this.finerioService.getJsonHeaders()}
+    ).map( (res: any ) => {
+      this.saveData( res.access_token, res.refresh_token, res.username );
+      this.getData();
+      return true;
+    });
   }
 }
