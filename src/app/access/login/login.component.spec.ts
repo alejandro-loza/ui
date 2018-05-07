3a23ba9dd8c3af961c5c8bdce3c710b203afee5e
Aarón Cervantes <aaron.cervantes@finerio.mx>
2018-05-07 09:56:30 -0500

    Created united test for login.component

--- src/app/access/login/login.component.spec.ts
+++ src/app/access/login/login.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+
+import { FormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login.component';
+import { AuthService, FinerioService } from '../../services/services.index';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +13,16 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        FormsModule
+      ],
+      providers: [
+        AuthService,
+        FinerioService
+      ]
     })
     .compileComponents();
   }));
