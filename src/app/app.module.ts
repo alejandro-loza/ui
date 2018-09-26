b5a2d45be20edf90c74c45b06bbb409ddb4b7abd
Aarón Cervantes <metallicaaron@hotmail.es>
2018-09-25 20:36:24 -0500

    Modified paths for imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from                             '@angular/core';
 import { BrowserModule } from                        '@angular/platform-browser';
 import { BrowserAnimationsModule } from              '@angular/platform-browser/animations';
-import { ServicesModule } from                       './services/services.module';
 
-import { APP_ROUTES } from                           './app.route';
+import { ServicesModule } from                       '@services/services.module';
 
-import { AppComponent } from                         './app.component';
-import { PagesComponent } from                       './pages/pages.component';
-import { AccessComponent } from                      './access/access.component';
-import { SharedModule } from                         './shared/shared.module';
+import { APP_ROUTES } from                           '@app/app.route';
+
+import { AppComponent } from                         '@app/app.component';
+import { PagesComponent } from                       '@pages/pages.component';
+import { AccessComponent } from                      '@access/access.component';
+import { SharedModule } from                         '@shared/shared.module';
 
 @NgModule({
   declarations: [
