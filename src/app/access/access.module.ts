2df91e74110e575ed9c09296aa10baaac4ef6413
Aarón Cervantes <metallicaaron@hotmail.es>
2018-09-25 20:40:01 -0500

    Modified paths for imports

--- src/app/access/access.module.ts
+++ src/app/access/access.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from           '@angular/core';
-import { ACCESS_ROUTES } from      './access.route';
-import { SharedModule } from       '../shared/shared.module';
+import { ACCESS_ROUTES } from      '@access/access.route';
+import { SharedModule } from       '@shared/shared.module';
 
 @NgModule({
   imports: [
     SharedModule,
     ACCESS_ROUTES
-  ]
+  ],
+  declarations: []
 })
 export class AccessModule { }
