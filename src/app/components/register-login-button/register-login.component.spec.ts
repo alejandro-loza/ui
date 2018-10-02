e5f2926f938153151330390ddf04506cf574563b
Aarón Cervantes <metallicaaron@hotmail.es>
2018-10-02 13:44:26 -0500

    Added new unit and integrate test

--- src/app/components/register-login-button/register-login.component.spec.ts
+++ src/app/components/register-login-button/register-login.component.spec.ts
@@ -1,14 +1,28 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// @angular Modules
+import { DebugElement } from                           '@angular/core';
+import { By } from                                     '@angular/platform-browser';
+// Testing @angular Imports
+import { async, ComponentFixture, TestBed } from       '@angular/core/testing';
+import { RouterTestingModule } from                    '@angular/router/testing';
+// Owns modules, components, sevices
+import { RegisterLoginComponent } from                 './register-login.component';
 
-import { RegisterLoginComponent } from './register-login.component';
 
 describe('RegisterLoginComponent', () => {
-  let component: RegisterLoginComponent;
-  let fixture: ComponentFixture<RegisterLoginComponent>;
+  let component:    RegisterLoginComponent,
+      fixture:      ComponentFixture<RegisterLoginComponent>,
+      buttonDe:     DebugElement,
+      buttonEl:     DebugElement,
+      expectButton;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterLoginComponent ]
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        RegisterLoginComponent
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +35,14 @@ describe('RegisterLoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create @inputs from button', () => {
+    buttonDe = fixture.debugElement.query(By.css('#registerLogin'));
+    buttonEl = buttonDe.nativeElement;
+
+    expectButton = { link: '/mock', text: 'Mock Button' };
+    component.link = expectButton.link;
+    component.textBtn = expectButton.text;
   });
 });
--- src/app/components/social-media-button/social-media.component.spec.ts
+++ src/app/components/social-media-button/social-media.component.spec.ts
@@ -1,14 +1,28 @@
+import { DebugElement } from                     '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SocialMediaComponent } from './social-media.component';
+import { SocialMediaComponent } from             './social-media.component';
+import { MaterializeModule } from                'ngx-materialize';
+import { By } from '@angular/platform-browser';
 
 describe('SocialMediaComponent', () => {
-  let component: SocialMediaComponent;
-  let fixture: ComponentFixture<SocialMediaComponent>;
+  let component: SocialMediaComponent,
+      fixture: ComponentFixture<SocialMediaComponent>,
+      FbDe:     DebugElement,
+      GglDe:     DebugElement,
+      FbEl:     DebugElement,
+      GglEl:     DebugElement,
+      expectFb,
+      expectGgl;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SocialMediaComponent ]
+      imports: [
+        MaterializeModule.forRoot()
+      ],
+      declarations: [
+        SocialMediaComponent
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +35,19 @@ describe('SocialMediaComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create @input Facebook', () => {
+    FbDe = fixture.debugElement.query(By.css('.facebook-color'));
+    FbEl = FbDe.nativeElement;
+    expectFb = { text: 'Dummy Fb text' };
+    component.facebookText = expectFb.text;
+  });
+
+  it('should create @input Google', () => {
+    GglDe = fixture.debugElement.query(By.css('.google-color'));
+    GglEl = FbDe.nativeElement;
+    expectGgl = { text: 'Dummy Google text' };
+    component.googleText = expectGgl.text;
   });
 });
