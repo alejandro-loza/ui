a8c3e75fb2b7aea1c5eb1a3d22b363b2fb58cbe3
Alan Vargas <alan.vargas@finerio.mx>
2018-10-09 12:14:21 -0500

    test of component

--- src/app/access/signup/components/signup.component.spec.ts
+++ src/app/access/signup/components/signup.component.spec.ts
@@ -1,25 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from            '@angular/common/http/testing';
+import { SocialMediaComponent } from               '@components/social-media-button/social-media.component';
+import { MaterializeModule } from                  'ngx-materialize';
+import { RouterTestingModule } from                '@angular/router/testing';
+
 
 import { SignupComponent } from './signup.component';
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
+  let socialMediaFix: ComponentFixture<SocialMediaComponent>;
+
+  let socialMediaComp: SocialMediaComponent;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignupComponent ]
+      declarations: [ SignupComponent, SocialMediaComponent ],
+      imports : [
+        HttpClientTestingModule,
+        MaterializeModule.forRoot(),
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SignupComponent);
+    socialMediaFix = TestBed.createComponent(SocialMediaComponent);
+
     component = fixture.componentInstance;
+    socialMediaComp = socialMediaFix.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create SocialMediaComponent', () => {
+    expect(socialMediaComp).toBeTruthy();
+  });
+
 });
