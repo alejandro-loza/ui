96c943d8537837d68f06d5e89261d8b198f5e2c7
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2018-10-15 00:05:59 -0500

    Added indexs cause it needs for multi-concepts

--- src/app/shared/interfaces/concept.interface.ts
+++ src/app/shared/interfaces/concept.interface.ts
@@ -1,10 +1,12 @@
 import { Category } from '@interfaces/category.interface';
 
 export interface Concept {
-    id: string;
-    amount: number;
-    description: string;
-    type: string;
-    movement: {};
-    category: Category;
+    [ index: number ]: {
+        id: string;
+        amount: number;
+        description: string;
+        type: string;
+        movement: {};
+        category: Category;
+    }
 }
