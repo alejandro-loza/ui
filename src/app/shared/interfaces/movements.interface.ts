ec5e86ef56de373858bcec60c178ea4c74111f8a
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2018-10-15 00:06:28 -0500

    Fixed concepts property

--- src/app/shared/interfaces/movements.interface.ts
+++ src/app/shared/interfaces/movements.interface.ts
@@ -2,22 +2,17 @@ import { Account } from './account.interfaces';
 import { Concept } from './concept.interface';
 
 export interface Movement {
-    [ index: number ]: {
-        id: string,
-        amount: number,
-        balance: number,
-        customDate: Date,
-        customDescription: string,
-        date: Date,
-        dateCreated: Date,
-        description: string,
-        duplicated: true,
-        lastUpdated: Date,
-        type: string,
-        account: Account,
-        [ index: number ]: {
-            concept: Concept
-        }
-    };
-    size: string;
+    id: string;
+    amount: number;
+    balance: number;
+    customDate: Date;
+    customDescription: string;
+    date: Date;
+    dateCreated: Date;
+    description: string;
+    duplicated: true;
+    lastUpdated: Date;
+    type: string;
+    account: Account;
+    concept: Concept;
 }
