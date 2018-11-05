b552ae22242b0c81d0b6b2440fe5aef14119d5c7
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2018-11-05 10:24:31 -0600

    Created movement interface

--- src/app/shared/interfaces/movements.interface.ts
+++ src/app/shared/interfaces/movements.interface.ts
@@ -1,18 +1,8 @@
-import { Account } from './account.interfaces';
-import { Concept } from './concept.interface';
+import { Movement } from '@interfaces/movement.interface';
 
-export interface Movement {
-    id: string;
-    amount: number;
-    balance: number;
-    customDate: Date;
-    customDescription: string;
-    date: Date;
-    dateCreated: Date;
-    description: string;
-    duplicated: true;
-    lastUpdated: Date;
-    type: string;
-    account: Account;
-    concept: Concept;
+export interface Movements {
+    data: {
+        [index: number]:  Movement
+    };
+    size: number;
 }
