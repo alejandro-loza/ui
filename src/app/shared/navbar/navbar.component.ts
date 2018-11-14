a37070562c73e9511804abd9ca49838df79469a3
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2018-11-14 12:16:53 -0600

    Fixed conflicts

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -3,10 +3,7 @@ import { Component,
          ViewChild,
          ElementRef } from              '@angular/core';
 import { Router } from                  '@angular/router';
-<<<<<<< Updated upstream
 import * as M from                      'materialize-css/dist/js/materialize';
-=======
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-navbar',
@@ -14,36 +11,21 @@ import * as M from                      'materialize-css/dist/js/materialize';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-<<<<<<< Updated upstream
   @ViewChild('sidenav') elemSidenav: ElementRef;
+  @ViewChild('collapsible') elemCollapsible: ElementRef;
   constructor(
     private router: Router
   ) { }
-=======
-  
-  constructor( private router: Router ) { }
->>>>>>> Stashed changes
 
   ngOnInit() {
     const instanceSidenav = new M.Sidenav(this.elemSidenav.nativeElement, {});
-    this.collapsableFun();
+    const instancesCollapsible = new M.Collapsible(this.elemCollapsible.nativeElement, {});
   }
 
-
-  collapsableFun() {
-    const chevron = document.querySelector('#chevron');
-    document.querySelector('#sidenav-collapsible').addEventListener('click', () => {
-      if (document.querySelector('.collapsible-header').classList.contains('active')) {
-        chevron.classList.replace('mdi-plus', 'mdi-chevron-down');
-      } else {
-        chevron.classList.replace('mdi-chevron-down', 'mdi-plus');
-      }
-    });
-  }
-  logout() {
-    sessionStorage.removeItem('access-token');
-    sessionStorage.removeItem('id-user');
-    sessionStorage.removeItem('refresh-token');
-    this.router.navigate(['/access/login']);
-  }
+  // logout() {
+  //   sessionStorage.removeItem('access-token');
+  //   sessionStorage.removeItem('id-user');
+  //   sessionStorage.removeItem('refresh-token');
+  //   this.router.navigate(['/access/login']);
+  // }
 }
