ee5d300cc6fbe8e393fe06194319f8451a72f2cc
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2018-11-25 15:39:41 -0600

    Redefined logic for movements, and pushed movement to a list

--- src/app/services/movements/movements.service.ts
+++ src/app/services/movements/movements.service.ts
@@ -6,86 +6,63 @@ import { ConfigService } from           '../config/config.service';
 
 import { map } from                     'rxjs/operators';
 
-import { QueryMovements } from          '@classes/queryMovementsDto.class';
+import { QueryMovements } from          '@app/shared/interfaces/queryMovements.interface';
 import { Movement } from                '@interfaces/movement.interface';
 import { Movements } from               '@interfaces/movements.interface';
 
 
 @Injectable()
 export class MovementsService {
-  movimientosObj: any;
-  movimientosList = [];
   private url = `${environment.backendUrl}/users`;
-  private id = this.configService.getId;
-  private token = this.configService.getToken;
-  public queryMovements: QueryMovements = new QueryMovements();
-  movementOrder: Movement[] = [];
-  lastMovement: Movement;
-  movementsOrderByDate: any = {};
-  options = { day: 'numeric', month: 'narrow', year: 'numeric' };
+  movementsList: Array<Movement>;
+
+
   constructor(
     private httpClient: HttpClient,
     private configService: ConfigService
-  ) {
-    this.queryMovements.setCharges = true;
-    this.queryMovements.setDeposits = true;
-    this.queryMovements.setDuplicates = true;
-    this.queryMovements.setDeep = true;
-  }
+  ) { }
 
-  allMovements (offset: number) {
+  /**
+   * @function allMovements Esta función lo que hace traer todos lo movimiento con los siguientes parametros
+   * @param offset : Esto es para obtener los siguientes movimientos
+   *
+   * Se inicializa el offset en 0, después de eso en la url se mandamn los siguientes parametros:
+   * @var url - Esta es la variable como se le enviara la url para la petición al servidor, todo los parametros son opcionales,
+   * pero se deben de tomar en cuenta.
+   * @param id : Este es el id del usuario
+   * @param deep : Si se quieres saber con detalle los movimiento
+   * @param max : El número máximo de movimientos que se piden
+   * @param duplicate : Para incluir los movimientos duplicaods
+   */
+
+  allMovements ( queryMovements: QueryMovements ) {
+    if ( queryMovements.offset === 0 ) {
+      this.movementsList = new Array();
+    }
+    const id = sessionStorage.getItem('id-user');
     const urlMovements =
-      `${this.url}/` +
-      `${this.id}/movements` +
-      `?deep=${this.queryMovements.getDeep}` +
-      `&offset=` + offset +
-      `&max=${this.queryMovements.getMax}` +
-      `&includeCharges=${this.queryMovements.getCharges}` +
-      `&includeDeposits=${this.queryMovements.getDeposits}` +
-      `&includeDuplicates=${this.queryMovements.getDuplicates}`
-      ;
-    this.queryMovements.setOffset = offset + this.queryMovements.getMax;
+      `${ this.url }/` +
+      `${ id }/movements` +
+      `?deep=${ queryMovements.deep }` +
+      `&offset=` + queryMovements.offset +
+      `&max=${ queryMovements.maxMovements }` +
+      `&includeCharges=${ queryMovements.charges }` +
+      `&includeDeposits=${ queryMovements.deposits }` +
+      `&includeDuplicates=${ queryMovements.duplicates }`;
     return this.httpClient.get<Movements>(
-             urlMovements,
-             { headers: this.configService.getJsonHeaders()}
-           ).pipe(
-             map((res: any) => {
- 1
-              for (let i = 0; i < res.data.length; i++) {
-                const movement: Movement = res.data[i];
-
-                /**
-                 * Agrupación por fechas
-                 */
-                this.movementsOrderByDate[
-                  new Date(movement.customDate).toLocaleDateString( navigator.language , {year: 'numeric', month: 'long', day: 'numeric'})
-                ] = this.movementOrder;
-
-                /**
-                 * Checa si es el último movimiento, y debe devolver la lista
-                 */
-                if ( movement === res.data[res.data.length - 1] ) {
-                  this.lastMovement = res.data[res.data.length - 1];
-                  console.log('%c Servicio log', 'color: yellow', this.movementsOrderByDate);
-                  return this.movementsOrderByDate;
-                }
-
-                /**
-                 * Se checa el movimiento actual con el siguiente para saber si tienen la misma fecha. Si es cumple esto, se agrupan,
-                 * sino se agrega el movimiento actual y se crea una nueva propiedad en el objeto con la fecha del siguiente movimiento
-                 */
-                if (
-                  (new Date(movement.date).getFullYear() && new Date(movement.date).getMonth() && new Date(movement.date).getDate())
-                  ===
-                  (new Date(res.data[i + 1].date).getFullYear() && new Date(res.data[i + 1].date).getMonth() && new Date(res.data[i + 1].date).getDate())
-                ) {
-                  this.movementOrder.push(movement);
-                } else {
-                  this.movementOrder.push(movement);
-                  this.movementOrder = new Array;
-                }
-              }
-             })
-           );
+      urlMovements,
+      { headers: this.configService.getJsonHeaders()}
+    ).pipe(
+      map( (res: Movements) => {
+        if ( this.movementsList.length === res.size) {
+          return;
+        }
+        for (let i = 0; i < res.data.length; i++) {
+          const movement: Movement = res.data[i];
+          this.movementsList.push(movement);
+        }
+        return this.movementsList;
+      })
+    );
   }
 }
