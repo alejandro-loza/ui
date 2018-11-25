ed8363bdc6e9e916e33792f6c8e2d1db40890bd5
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2018-11-25 13:22:15 -0600

    Added url for refresh-token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   backendUrl: 'https://api.finerio.mx',
   newBackendUrl: 'https://api-v2.finerio.mx',
-  apiUrl: 'https://api.finerio.mx/'
+  apiUrl: 'https://api.finerio.mx'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   backendUrl: 'https://api.finerio.mx/api',
   newBackendUrl: 'https://api-v2.finerio.mx',
-  apiUrl: 'https://api.finerio.mx/'
+  apiUrl: 'https://api.finerio.mx'
 };
 
 /*
