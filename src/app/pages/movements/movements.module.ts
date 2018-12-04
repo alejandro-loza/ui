bab269bc60ec94b8a224b92e5b26a97fc0dd3e66
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2018-12-04 16:42:53 -0600

    Changed reactiveForms for ngForm and added new-component.component

--- src/app/pages/movements/movements.module.ts
+++ src/app/pages/movements/movements.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from                           '@angular/core';
-import { ReactiveFormsModule } from                '@angular/forms';
+import { FormsModule } from                '@angular/forms';
 import { SharedModule } from                       '@shared/shared.module';
 
 import { MovementModule } from                     './components/movement/movement.module';
@@ -9,11 +9,12 @@ import { MovementsRoutes } from                    './movements.route';
 import { SpinnerComponent } from                   '@components/spinner/spinner.component';
 import { MovementsComponent } from                 './components/movements.component';
 import { MovementDetailMedAndUpComponent } from    './components/movement-detail-med-and-up/movement-detail-med-and-up.component';
+import { NewMovementComponent } from               './components/new-movement/new-movement.component';
 
 
 @NgModule({
   imports: [
-    ReactiveFormsModule,
+    FormsModule,
     SharedModule,
     MovementModule,
     MovementsRoutes,
@@ -22,6 +23,7 @@ import { MovementDetailMedAndUpComponent } from    './components/movement-detail
     MovementsComponent,
     SpinnerComponent,
     MovementDetailMedAndUpComponent,
+    NewMovementComponent,
   ]
 })
 export class MovementsModule { }
