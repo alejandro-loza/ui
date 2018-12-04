7dd0b95f5903d5b7f9cdfca1330e4bdc7fba8ab9
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2018-12-04 16:45:20 -0600

    Erased dto and new-movement component. it was moved

--- src/app/pages/movements/new-movement/new-movement.routes.ts
+++
@@ -1,8 +0,0 @@
-import { RouterModule, Routes } from '@angular/router';
-import { NewMovementComponent } from './new-movement/new-movement.component';
-
-const NewMovementsRouting: Routes = [
-    { path: '', component: NewMovementComponent }
-];
-
-export const NewMovementsRoutes = RouterModule.forChild( NewMovementsRouting );
