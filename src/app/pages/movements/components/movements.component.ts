05d45378520a552558c46350dca8a57ec6f6143a
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2018-12-04 16:56:33 -0600

    Used angular to init collapsible element and get its instance. Also toast when u got all the movements, and created a function for evetEmmitter

--- src/app/pages/movements/components/movements.component.ts
+++ src/app/pages/movements/components/movements.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+  AfterViewInit
+} from '@angular/core';
 
-import { MovementsService } from    '@services/services.index';
-import { ConfigService } from       '@services/config/config.service';
+import { MovementsService } from '@services/services.index';
+import { ConfigService } from '@services/config/config.service';
 
-import { ParamsMovements } from      '@app/shared/interfaces/paramsMovements.interface';
-import { Movement } from            '@interfaces/movement.interface';
+import { ParamsMovements } from '@app/shared/interfaces/paramsMovements.interface';
+import { Movement } from '@interfaces/movement.interface';
 
-import * as M from                  'materialize-css/dist/js/materialize';
+import * as M from 'materialize-css/dist/js/materialize';
 declare const $: any;
 
 @Component({
@@ -26,15 +33,14 @@ export class MovementsComponent implements OnInit, AfterViewInit {
     deposits: true,
     duplicates: true,
     offset: 0,
-    maxMovements: 35,
+    maxMovements: 35
   };
-
 
   constructor(
     private movementsService: MovementsService,
     private configService: ConfigService,
     public renderer: Renderer2
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.getMovements(this.paramsMovements);
@@ -42,8 +48,13 @@ export class MovementsComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    const initCollapsible = new M.Collapsible(this.elcollapsible.nativeElement, {});
-    const instanceCollapsible = M.Collapsible.getInstance(this.elcollapsible.nativeElement);
+    const initCollapsible = new M.Collapsible(
+      this.elcollapsible.nativeElement,
+      {}
+    );
+    const instanceCollapsible = M.Collapsible.getInstance(
+      this.elcollapsible.nativeElement
+    );
   }
 
   getMovements(paramsMovements: ParamsMovements) {
@@ -52,10 +63,9 @@ export class MovementsComponent implements OnInit, AfterViewInit {
         this.movementsList = res;
       },
       (err: any) => {
-        if ( err.status === 401 ) {
+        if (err.status === 401) {
           this.configService.refreshToken();
-          const toastHTML =
-            `<span>Hemos actualizado tu sesión, ¡Bienvenido de nuevo!</span>
+          const toastHTML = `<span>Hemos actualizado tu sesión, ¡Bienvenido de nuevo!</span>
             <button class="btn-flat toast-action" onClick="
               const toastElement = document.querySelector('.toast');
               const toastInstance = M.Toast.getInstance(toastElement);
@@ -68,14 +78,12 @@ export class MovementsComponent implements OnInit, AfterViewInit {
             displayLength: 2000
           });
         } else {
-          console.error(err);
-          const toastHTML =
-            `<span>¡Ha ocurrido un error al obterner tus movimiento!</span>
+          const toastHTML = `<span>¡Ha ocurrido un error al obterner tus movimiento!</span>
             <button class="btn-flat toast-action" onClick="
               const toastElement = document.querySelector('.toast');
               const toastInstance = M.Toast.getInstance(toastElement);
               toastInstance.dismiss();">
-            <i class="mdi mdi-24px mdi-close grey-text text-lighten-4 right"><i/>
+              <i class="mdi mdi-24px mdi-close grey-text text-lighten-4 right"><i/>
             </button>`;
           M.toast({
             html: toastHTML,
@@ -83,21 +91,40 @@ export class MovementsComponent implements OnInit, AfterViewInit {
             displayLength: 2000
           });
         }
-      }, () => {
+      },
+      () => {
         this.renderer.setStyle(this.elSpinner.nativeElement, 'display', 'none');
-      });
-      this.paramsMovements.offset = this.paramsMovements.offset + this.paramsMovements.maxMovements;
+      }
+    );
+    this.paramsMovements.offset =
+      this.paramsMovements.offset + this.paramsMovements.maxMovements;
   }
 
   offsetMovement() {
-    window.addEventListener('scroll', () => {
-      const scrollVertical = window.scrollY;
-      this.scrollLimit = ($(document).height() - $(window).height());
-      if ( scrollVertical >= this.scrollLimit) {
-        this.renderer.setStyle(this.elSpinner.nativeElement, 'display', 'block');
-        this.getMovements(this.paramsMovements);
-      }
-    }, true);
+    window.addEventListener(
+      'scroll',
+      () => {
+        const scrollVertical = window.scrollY;
+        this.scrollLimit = $(document).height() - $(window).height();
+        if (scrollVertical >= this.scrollLimit) {
+          this.renderer.setStyle(
+            this.elSpinner.nativeElement,
+            'display',
+            'block'
+          );
+          this.getMovements(this.paramsMovements);
+        }
+      },
+      true
+    );
     this.renderer.setStyle(this.elSpinner.nativeElement, 'display', 'none');
+  }
+
+  movementActionEmit(flag: boolean) {
+    if (flag === true) {
+      this.paramsMovements.offset = 0;
+      this.movementsList = [];
+      this.getMovements(this.paramsMovements);
+    }
   }
 }
