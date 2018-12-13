8888f998664903d640b4bc0c0d03c8a7fd87b4b7
Alan Vargas <alan.vargas@finerio.mx>
2018-12-13 15:59:05 -0600

    using reactive Forms

--- src/app/access/signup/components/signup.component.ts
+++ src/app/access/signup/components/signup.component.ts
@@ -1,10 +1,9 @@
 import { Component } from                   '@angular/core';
 import { SignupService } from               '../../../services/signup/signup.service';
 import { Router } from                      '@angular/router';
-import { isNullOrUndefined } from           'util';
-import { SignupData } from                  '@shared/dto/signupDto';
-import { NgForm } from                      '@angular/forms';
-declare const $: any;
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import * as M from 'materialize-css/dist/js/materialize';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -14,69 +13,44 @@ declare const $: any;
 
 export class SignupComponent {
 
-  passwordValidate:boolean = false;
+  passwordValidate:boolean = true;
+  termsAccepted:boolean = true;
+  signupData = new FormGroup({
+    email : new FormControl(),
+    password: new FormControl(),
+    passwordConfirm: new FormControl(),
+    blog : new FormControl( true ),
+    terms: new FormControl( { value: true }, Validators.required )
+  });
   
   constructor( private _signupService: SignupService,
                private router:Router ) { }
 
-  signup( SignupForm: NgForm ) {
-    console.log(SignupForm.value);
-    const data = new SignupData(
-      SignupForm.value.email,
-      SignupForm.value.password,
-      SignupForm.value.confirmPassword,
-      SignupForm.value.termsAndConditions,
-      SignupForm.value.blog
-    );
-
-    this.limpiaErrorSpan();
-    if( !isNullOrUndefined(data.email) || !isNullOrUndefined(data.password) ){
-      this.passwordMatch( data.password, data.passwordConfirm );
-
-      if ( this.passwordValidate ) {
-        if( data.termsAndConditions ){
-
-          this._signupService.signup( data ).subscribe( res => {
-            console.log(res);
-             this.router.navigate(['/access/login']);
-             
-           }, error => {
-               let errorMessage = error.error.message;
-                 $("#errorSpan").html( `${ errorMessage }` ).css({
-                   'color':'red',
-                   'font-size':'12px'
-                 }); 
-               
-             }   
-           );
-        } else {
-          $("#errorSpan").html("Debes aceptar los término y condiciones para continuar").css({
-            'color':'red',
-            'font-size':'12px'
-          });
-        }
-      } else {
-        $("#errorSpan").html("Contraseñas no coinciden").css({
-          'color':'red',
-          'font-size':'12px'
+  signup( ) {
+    this.passwordMatch();
+    this.termsValidate();
+    if( this.passwordValidate && this.termsAccepted ){
+      this._signupService.signup( this.signupData.value ).subscribe( res => {
+        this.router.navigate(['/access/login']);
+      }, error => {
+         M.toast({
+           html: error.error.message,
+           displayLength: 1500
         });
-      }
-    } else {
-      $("#errorSpan").html("Correo y contraseña son necesarios").css({
-        'color':'red',
-        'font-size':'12px'
-      });
+      })
     }
   }
 
-  passwordMatch( password, confirmPassword ){
-    password   == confirmPassword ?
-      this.passwordValidate = true : 
-      this.passwordValidate = false
+  passwordMatch(){
+    this.signupData.value.password === this.signupData.value.passwordConfirm ?
+    this.passwordValidate = true : 
+    this.passwordValidate = false
   }
 
-  limpiaErrorSpan(){
-    $("#errorSpan").html("");
+  termsValidate(){
+    this.signupData.value.terms ? 
+    this.termsAccepted = true :
+    this.termsAccepted = false
   }
 }
 
