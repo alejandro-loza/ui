96866e59eb4f5e1e73a4dcca7e7bf18e206fd760
Alan Vargas <alan.vargas@finerio.mx>
2018-12-17 15:22:06 -0600

    new backendURL for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://api.finerio.mx',
+  backendUrl: 'https://api.finerio.mx/api',
   newBackendUrl: 'https://api-v2.finerio.mx',
   apiUrl: 'https://api.finerio.mx'
 };
