c5c00e7385a78f8f68d3c7f97f1505076a5e1a0f
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2018-12-19 23:15:05 -0600

    Added tooltips for icons and hide for large text for desktop devices

--- src/app/shared/navbar/link/link.component.ts
+++ src/app/shared/navbar/link/link.component.ts
@@ -1,24 +1,34 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import * as M from 'materialize-css/dist/js/materialize';
 @Component({
   selector: 'app-link',
   templateUrl: './link.component.html',
   styleUrls: ['./link.component.css']
 })
-export class LinkComponent implements OnInit {
+export class LinkComponent implements OnInit, AfterViewInit {
   @Input() route: string;
   @Input() nombre: string;
   @Input() icon: string;
   @Input() show: boolean;
+
   @Output() showValue: EventEmitter<boolean>;
+
+  @ViewChild('tooltip') elementTooltip: ElementRef;
+
   constructor() {
     this.show = false;
     this.showValue = new EventEmitter();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  ngAfterViewInit() {
+    const initTooltip = new M.Tooltip(this.elementTooltip.nativeElement, {
+      enterDelay: 300,
+      outDuration: 150,
+      transitionMovement: 0
+    });
+  }
   showValueEmit() {
     this.showValue.emit(this.show);
   }
