165effb15384de109c647ef1f12a91a5a6a3e714
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2018-12-19 23:15:18 -0600

    Erased saving component, module and route

--- src/app/pages/saving/saving.route.ts
+++
@@ -1,9 +0,0 @@
-import { RouterModule, Routes } from '@angular/router';
-
-import { SavingComponent } from './components/saving.component';
-
-const SAVING_ROUTING: Routes = [
-    { path: '', component: SavingComponent }
-];
-
-export const SAVING_ROUTES = RouterModule.forChild(SAVING_ROUTING);
