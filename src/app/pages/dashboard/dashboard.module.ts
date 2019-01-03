c5a8da36a2c7a20e6edddd6700c552f38566235f
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-01-03 13:58:59 -0600

    Modified routes to get lazyModule for each tab on dashboard

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,40 +1,31 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+
 import { SharedModule } from '@shared/shared.module';
+
+// Service
+import { MovementsService } from '@services/movements/movements.service';
 
 // Component
 import { DashboardComponent } from   './component/dashboard.component';
 import { TabsComponent } from        './component/tabs/tabs.component';
 
-import { BalanceModule } from        './balance/balance.module';
-import { DiagnosticModule } from     './diagnostic/diagnostic.module';
-import { ExpensesModule } from       './expenses/expenses.module';
-import { IncomesModule } from        './incomes/incomes.module';
-
 // Routes
 import { DashboardRoutes } from      './dashboard.route';
 import { MonthChartComponent } from  './component/monthChart/month-chart.component';
 
-import { MovementsService } from     '@services/movements/movements.service';
+
 
 @NgModule({
   imports: [
-    CommonModule,
-    RouterModule,
-    BalanceModule,
-    DiagnosticModule,
-    ExpensesModule,
-    IncomesModule,
     SharedModule,
     NgxChartsModule,
     DashboardRoutes,
   ],
   declarations: [
-    DashboardComponent,
     TabsComponent,
     MonthChartComponent,
+    DashboardComponent,
   ],
   providers: [ MovementsService ]
 })
