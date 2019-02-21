301557de17b93eb77c59f65e9570ede9914dc9ab
Alan Vargas <alanvhz@MacBook-Pro-de-Alan.local>
2019-02-21 13:59:39 -0600

    build for dashbaord solved

--- src/app/pages/dashboard/component/dashboard.component.ts
+++ src/app/pages/dashboard/component/dashboard.component.ts
@@ -8,120 +8,121 @@ import { CategoriesService } from '@services/categories/categories.service';
 import { Category } from '@interfaces/category.interface';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+	selector: 'app-dashboard',
+	templateUrl: './dashboard.component.html',
+	styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
-  paramsMovements = {
-    charges: true,
-    deep: true,
-    deposits: true,
-    startDate: '',
-    endDate: '',
-    duplicates: false,
-    maxMovements: 150,
-    offset: 0
-  };
+	paramsMovements = {
+		charges: true,
+		deep: true,
+		deposits: true,
+		startDate: '',
+		endDate: '',
+		duplicates: false,
+		maxMovements: 150,
+		offset: 0
+	};
 
-  movementsList: Movement[] = [];
-  movementsServiceResponse: Movement[];
-  categoriesList: Category[] = [];
-  dataReady: boolean = false;
+	movementsList: Movement[] = [];
+	movementsServiceResponse: Movement[];
+	categoriesList: Category[] = [];
+	dataReady: boolean = false;
+	showEmptyState: boolean = this.dashboardBean.getShowEmptyState();
 
-  /*0 INCOMES 1 EXPENSES 2 BALANCE */
-  tabSelected: number = 1;
+	/*0 INCOMES 1 EXPENSES 2 BALANCE */
+	tabSelected: number = 1;
 
-  constructor(
-    private movementsService: MovementsService,
-    private dateApi: DateApiService,
-    private dashboardService: DashboardService,
-    private categoriesService: CategoriesService,
-    private dashboardBean: DashboardBeanService
-  ) {}
+	constructor(
+		private movementsService: MovementsService,
+		private dateApi: DateApiService,
+		private dashboardService: DashboardService,
+		private categoriesService: CategoriesService,
+		private dashboardBean: DashboardBeanService
+	) {}
 
-  ngOnInit() {
-    if( !this.dashboardBean.getShowEmptyState() ){
-      if (this.dashboardBean.getLoadInformation()) {
-        this.getCategoriesInfo();
-      } else {
-        this.dataReadyValidator();
-      }
-    } else {
-      this.dataReady = true;
-    }
-  }
+	ngOnInit() {
+		if (!this.dashboardBean.getShowEmptyState()) {
+			if (this.dashboardBean.getLoadInformation()) {
+				this.getCategoriesInfo();
+			} else {
+				this.dataReadyValidator();
+			}
+		} else {
+			this.dataReady = true;
+			this.showEmptyState = true;
+		}
+	}
 
-  getMovementsData(categories: Category[]) {
-    this.movementsService.getMovements(this.paramsMovements).subscribe(
-      res => {
-        this.movementsServiceResponse = res.body.data;
-      },
-      error => {},
-      () => {
-        this.paramsMovements.offset += 150;
-        if (
-          this.movementsServiceResponse.length == this.paramsMovements.offset
-        ) {
-          this.getMovementsData(categories);
-        } else {
-          this.movementsServiceResponse.forEach(movement => {
-            this.movementsList.push(movement);
-          });
-          if( this.movementsList.length > 0){
-            this.dashboardService.mainMethod( this.movementsList, this.categoriesList );
-          } else {
-            this.dashboardBean.setShowEmptyState( true );
-            this.dataReady = true;
-          }
-        }
-      }
-    );
-    this.dataReadyValidator();
-  }
+	getMovementsData(categories: Category[]) {
+		this.movementsService.getMovements(this.paramsMovements).subscribe(
+			(res) => {
+				this.movementsServiceResponse = res.body.data;
+			},
+			(error) => {},
+			() => {
+				this.paramsMovements.offset += 150;
+				if (this.movementsServiceResponse.length == this.paramsMovements.offset) {
+					this.getMovementsData(categories);
+				} else {
+					this.movementsServiceResponse.forEach((movement) => {
+						this.movementsList.push(movement);
+					});
+					if (this.movementsList.length > 0) {
+						this.dashboardService.mainMethod(this.movementsList, this.categoriesList);
+					} else {
+						this.dashboardBean.setShowEmptyState(true);
+						this.showEmptyState = true;
+						this.dataReady = true;
+					}
+				}
+			}
+		);
+		this.dataReadyValidator();
+	}
 
-  getCategoriesInfo() {
-    this.categoriesList = [];
-    this.dashboardBean.setLoadInformation(false);
-    this.categoriesService.getCategoriesInfo().subscribe(res => {
-      res.body.data.forEach((category: Category) => {
-        this.categoriesList.push(category);
-      });
-      this.getDatesForParams();
-      this.getMovementsData(this.categoriesList);
-    });
-  }
+	getCategoriesInfo() {
+		this.categoriesList = [];
+		this.dashboardBean.setLoadInformation(false);
+		this.categoriesService.getCategoriesInfo().subscribe((res) => {
+			res.body.data.forEach((category: Category) => {
+				this.categoriesList.push(category);
+			});
+			this.getDatesForParams();
+			this.getMovementsData(this.categoriesList);
+		});
+	}
 
-  tabClicked(event) {
-    this.tabSelected = event;
-  }
+	tabClicked(event) {
+		this.tabSelected = event;
+	}
 
-  dataReadyValidator() {
-    if( !this.dashboardBean.getShowEmptyState() ){
-      if (this.dashboardBean.getDataIsReady()) {
-        this.dataReady = true;
-      } else {
-        setTimeout(() => {
-          this.dataReadyValidator();
-        }, 200);
-      }
-    }
-  }
+	dataReadyValidator() {
+		if (!this.dashboardBean.getShowEmptyState()) {
+			if (this.dashboardBean.getDataIsReady()) {
+				this.dataReady = true;
+			} else {
+				setTimeout(() => {
+					this.dataReadyValidator();
+				}, 200);
+			}
+		}
+	}
 
-  getDatesForParams() {
-    let currentDate = new Date();
-    // END DATE
-    let endDate = this.dateApi.dateWithFormat(currentDate);
-    this.paramsMovements.endDate = endDate;
+	getDatesForParams() {
+		let currentDate = new Date();
+		// END DATE
+		let endDate = this.dateApi.dateWithFormat(currentDate);
+		this.paramsMovements.endDate = endDate;
 
-    // START DATE
-    let startDateAux = new Date();
-    startDateAux.setMonth(startDateAux.getMonth() - 11);
-    startDateAux.setDate(1);
-    startDateAux.setHours(0);
-    startDateAux.setMinutes(0);
-    startDateAux.setMilliseconds(0);
-    let startDate = this.dateApi.dateWithFormat(startDateAux);
-    this.paramsMovements.startDate = startDate;
-  }
+		// START DATE
+		let startDateAux = new Date();
+		startDateAux.setMonth(startDateAux.getMonth() - 11);
+		startDateAux.setDate(1);
+		startDateAux.setHours(0);
+		startDateAux.setMinutes(0);
+		startDateAux.setMilliseconds(0);
+		let startDate = this.dateApi.dateWithFormat(startDateAux);
+		this.paramsMovements.startDate = startDate;
+	}
 }
