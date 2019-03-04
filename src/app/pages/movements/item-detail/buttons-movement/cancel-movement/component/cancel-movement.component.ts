242e14abba7462341ae8e133430f215255c4e550
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-03-04 13:15:24 -0600

    Created edit-movement button

---
+++ src/app/pages/movements/item-detail/buttons-movement/cancel-movement/component/cancel-movement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cancel-movement',
+  templateUrl: './cancel-movement.component.html',
+  styleUrls: ['./cancel-movement.component.css']
+})
+export class CancelMovementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
