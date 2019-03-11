280c4c8448112b37a1cce969e633ee95ae52ee35
Alan Vargas <alan.vargas@finerio.mx>
2019-03-11 11:50:52 -0600

    Dashboard refresh done

--- src/app/pages/movements/item-detail/buttons-movement/save-movement/component/save-movement.component.ts
+++ src/app/pages/movements/item-detail/buttons-movement/save-movement/component/save-movement.component.ts
@@ -1,85 +1,81 @@
-import {
-  Component,
-  OnInit,
-  Input,
-  Output,
-  EventEmitter,
-  OnChanges
-} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 import { MovementsService } from '@services/movements/movements.service';
 import { ToastService } from '@services/toast/toast.service';
 import { ToastInterface } from '@interfaces/toast.interface';
 import { Movement } from '@interfaces/movement.interface';
+import { CleanerService } from '@services/cleaner/cleaner.service';
 import { isNull } from 'util';
 
 @Component({
-  selector: 'app-save-movement',
-  templateUrl: './save-movement.component.html',
-  styleUrls: ['./save-movement.component.css']
+	selector: 'app-save-movement',
+	templateUrl: './save-movement.component.html',
+	styleUrls: [ './save-movement.component.css' ]
 })
 export class SaveMovementComponent implements OnInit, OnChanges {
-  @Input() movement: Movement;
-  @Input() keyEnter: boolean;
-  @Output() status: EventEmitter<boolean>;
-  @Output() keyEnterPressed: EventEmitter<boolean>;
+	@Input() movement: Movement;
+	@Input() keyEnter: boolean;
+	@Output() status: EventEmitter<boolean>;
+	@Output() keyEnterPressed: EventEmitter<boolean>;
 
-  auxAmount: number;
+	auxAmount: number;
 
-  toastInterface: ToastInterface;
+	toastInterface: ToastInterface;
 
-  constructor(
-    private movementService: MovementsService,
-    private toastService: ToastService
-  ) {
-    this.status = new EventEmitter();
-    this.keyEnterPressed = new EventEmitter();
-    this.toastInterface = {};
-  }
+	constructor(
+		private movementService: MovementsService,
+		private toastService: ToastService,
+		private cleanerService: CleanerService
+	) {
+		this.status = new EventEmitter();
+		this.keyEnterPressed = new EventEmitter();
+		this.toastInterface = {};
+	}
 
-  ngOnInit() { }
+	ngOnInit() {}
 
-  ngOnChanges(): void {
-    if ( this.keyEnter === true ) {
-      this.updateMovement();
-    }
-  }
+	ngOnChanges(): void {
+		if (this.keyEnter === true) {
+			this.updateMovement();
+		}
+	}
 
-  updateMovement() {
-    if ( this.movement.customDescription === '' || this.movement.customDescription === null ) {
-      this.movement.customDescription = this.movement.description;
-    }
-    if ( isNull( this.movement.customDate ) ) {
-      this.movement.customDate = this.movement.date;
-    }
-    if ( isNull(this.movement.customAmount) ) {
-      this.movement.customAmount = this.movement.amount;
-    }
-    this.movementService.updateMovement(this.movement).subscribe(
-      res => {
-        this.toastInterface.code = res.status;
-      },
-      err => {
-        this.toastInterface.code = err.status;
-        if (err.status === 401) {
-          this.toastService.toastGeneral(this.toastInterface);
-          this.updateMovement();
-        }
-        if (err.status === 404) {
-          this.toastInterface.message = 'No sé encontró tu movimiento';
-          this.toastService.toastGeneral(this.toastInterface);
-        }
-        if (err.status === 500) {
-          this.toastInterface.message =
-            '¡Ha ocurrido un error al obterner tus movimiento!';
-          this.toastService.toastGeneral(this.toastInterface);
-        }
-      },
-      () => {
-        this.toastInterface.message = 'Se actualizó su movimiento exitosamente';
-        this.toastService.toastGeneral(this.toastInterface);
-        this.status.emit(true);
-        this.keyEnterPressed.emit(false);
-      }
-    );
-  }
+	updateMovement() {
+		if (this.movement.customDescription === '' || this.movement.customDescription === null) {
+			this.movement.customDescription = this.movement.description;
+		}
+		if (isNull(this.movement.customDate)) {
+			this.movement.customDate = this.movement.date;
+		}
+		if (isNull(this.movement.customAmount)) {
+			this.movement.customAmount = this.movement.amount;
+		}
+		this.movementService.updateMovement(this.movement).subscribe(
+			(res) => {
+				this.toastInterface.code = res.status;
+			},
+			(err) => {
+				this.toastInterface.code = err.status;
+				if (err.status === 401) {
+					this.toastService.toastGeneral(this.toastInterface);
+					this.updateMovement();
+				}
+				if (err.status === 404) {
+					this.toastInterface.message = 'No sé encontró tu movimiento';
+					this.toastService.toastGeneral(this.toastInterface);
+				}
+				if (err.status === 500) {
+					this.toastInterface.message = '¡Ha ocurrido un error al obterner tus movimiento!';
+					this.toastService.toastGeneral(this.toastInterface);
+				}
+			},
+			() => {
+				this.cleanerService.cleanBudgetsVariables();
+				this.cleanerService.cleanDashboardVariables();
+				this.toastInterface.message = 'Se actualizó su movimiento exitosamente';
+				this.toastService.toastGeneral(this.toastInterface);
+				this.status.emit(true);
+				this.keyEnterPressed.emit(false);
+			}
+		);
+	}
 }
--- src/app/pages/movements/new-movement/component/new-movement.component.ts
+++ src/app/pages/movements/new-movement/component/new-movement.component.ts
@@ -1,17 +1,18 @@
 import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  EventEmitter,
-  OnInit,
-  Output,
-  ViewChild,
-  Renderer2
+	AfterViewInit,
+	Component,
+	ElementRef,
+	EventEmitter,
+	OnInit,
+	Output,
+	ViewChild,
+	Renderer2
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { MovementsService } from '@services/movements/movements.service';
 import { ToastService } from '@services/toast/toast.service';
+import { CleanerService } from '@services/cleaner/cleaner.service';
 
 import { ToastInterface } from '@interfaces/toast.interface';
 import { NewMovement } from '@interfaces/newMovement.interface';
@@ -20,87 +21,89 @@ import * as M from 'materialize-css/dist/js/materialize';
 import { retry } from 'rxjs/operators';
 import { DateApiService } from '@services/date-api/date-api.service';
 @Component({
-  selector: 'app-new-movement',
-  templateUrl: './new-movement.component.html',
-  styleUrls: ['./new-movement.component.css']
+	selector: 'app-new-movement',
+	templateUrl: './new-movement.component.html',
+	styleUrls: [ './new-movement.component.css' ]
 })
 export class NewMovementComponent implements OnInit, AfterViewInit {
-  @ViewChild('duplicated') checkboxDuplicate: ElementRef;
-  @ViewChild('modal') modalElement: ElementRef;
-  @ViewChild('datepicker') elDatePickker: ElementRef;
-  @ViewChild('btnSubmit') buttonSubmit: ElementRef;
+	@ViewChild('duplicated') checkboxDuplicate: ElementRef;
+	@ViewChild('modal') modalElement: ElementRef;
+	@ViewChild('datepicker') elDatePickker: ElementRef;
+	@ViewChild('btnSubmit') buttonSubmit: ElementRef;
 
-  @Output() createMovementStatus: EventEmitter<boolean>;
+	@Output() createMovementStatus: EventEmitter<boolean>;
 
-  newMovement: NewMovement;
-  formatDate: string;
-  date: Date;
-  toastInterface: ToastInterface;
-  instaceModal;
+	newMovement: NewMovement;
+	formatDate: string;
+	date: Date;
+	toastInterface: ToastInterface;
+	instaceModal;
 
-  constructor(
-    private movementService: MovementsService,
-    private dateApiService: DateApiService,
-    private toastService: ToastService,
-    private renderer: Renderer2
-  ) {
-    this.newMovement = {
-      date: this.dateApiService.dateApi(new Date()),
-      type: 'charge'
-    };
-    this.date = new Date();
-    this.createMovementStatus = new EventEmitter();
-    this.toastInterface = { code: null, message: null };
-  }
+	constructor(
+		private movementService: MovementsService,
+		private dateApiService: DateApiService,
+		private toastService: ToastService,
+		private cleanerService: CleanerService,
+		private renderer: Renderer2
+	) {
+		this.newMovement = {
+			date: this.dateApiService.dateApi(new Date()),
+			type: 'charge'
+		};
+		this.date = new Date();
+		this.createMovementStatus = new EventEmitter();
+		this.toastInterface = { code: null, message: null };
+	}
 
-  ngOnInit() {
-    this.formatDate = this.dateApiService.dateFormatMovement(this.date);
-  }
+	ngOnInit() {
+		this.formatDate = this.dateApiService.dateFormatMovement(this.date);
+	}
 
-  ngAfterViewInit() {
-    const initModal = new M.Modal(this.modalElement.nativeElement, {});
-    this.instaceModal = M.Modal.getInstance(this.modalElement.nativeElement);
-  }
+	ngAfterViewInit() {
+		const initModal = new M.Modal(this.modalElement.nativeElement, {});
+		this.instaceModal = M.Modal.getInstance(this.modalElement.nativeElement);
+	}
 
-  createMovement(form: NgForm) {
-    form.value.date = this.newMovement.date;
-    form.value.type = this.newMovement.type;
-    this.renderer.addClass(this.buttonSubmit.nativeElement, 'disabled');
-    this.movementService.createMovement(form.value).subscribe(
-      res => {
-        this.createMovementStatus.emit(true);
-        this.toastInterface.code = res.status;
-      },
-      err => {
-        this.toastInterface.code = err.status;
-        if (err.status === 401) {
-          this.toastService.toastGeneral(this.toastInterface);
-          this.createMovement(form);
-        }
-        if (err.status === 500) {
-          this.toastInterface.message =
-            '¡Ha ocurrido un error al obterner tus movimiento!';
-          this.toastService.toastGeneral(this.toastInterface);
-        }
-      },
-      () => {
-        this.instaceModal.close();
-        this.renderer.removeClass(this.buttonSubmit.nativeElement, 'disabled');
-        form.resetForm();
-        this.toastInterface.message = 'Se creó su movimiento exitosamente';
-        this.toastService.toastGeneral(this.toastInterface);
-      }
-    );
-  }
+	createMovement(form: NgForm) {
+		form.value.date = this.newMovement.date;
+		form.value.type = this.newMovement.type;
+		this.renderer.addClass(this.buttonSubmit.nativeElement, 'disabled');
+		this.movementService.createMovement(form.value).subscribe(
+			(res) => {
+				this.createMovementStatus.emit(true);
+				this.toastInterface.code = res.status;
+			},
+			(err) => {
+				this.toastInterface.code = err.status;
+				if (err.status === 401) {
+					this.toastService.toastGeneral(this.toastInterface);
+					this.createMovement(form);
+				}
+				if (err.status === 500) {
+					this.toastInterface.message = '¡Ha ocurrido un error al obterner tus movimiento!';
+					this.toastService.toastGeneral(this.toastInterface);
+				}
+			},
+			() => {
+				this.cleanerService.cleanBudgetsVariables();
+				this.cleanerService.cleanDashboardVariables();
+				this.instaceModal.close();
+				this.renderer.removeClass(this.buttonSubmit.nativeElement, 'disabled');
+				form.resetForm();
+				this.toastInterface.message = 'Se creó su movimiento exitosamente';
+				this.toastService.toastGeneral(this.toastInterface);
+			}
+		);
+	}
 
-  valueType(type: string) {
-    if (type === 'deposit') {
-      document.getElementById('charge').classList.remove('active');
-      document.getElementById('deposit').classList.add('active');
-    } else {
-      document.getElementById('deposit').classList.remove('active');
-      document.getElementById('charge').classList.add('active');
-    }
-    this.newMovement.type = type;
-  }
+	valueType(type: string) {
+		if (type === 'deposit') {
+			document.getElementById('charge').classList.remove('active');
+			document.getElementById('deposit').classList.add('active');
+		} else {
+			document.getElementById('deposit').classList.remove('active');
+			document.getElementById('charge').classList.add('active');
+		}
+		this.newMovement.type = type;
+	}
 }
