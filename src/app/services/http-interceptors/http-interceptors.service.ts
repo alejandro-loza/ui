4990043fc0db09f8485dfc34d9d590405d0a9aa9
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-03-13 18:55:56 -0600

    Created http-interceptor for token

---
+++ src/app/services/http-interceptors/http-interceptors.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HTTP_INTERCEPTORS,
+  HttpErrorResponse
+} from '@angular/common/http';
+
+import { ConfigService } from '../config/config.service';
+
+import { JWT } from '@interfaces/jwt.interface';
+
+import { Observable, throwError } from 'rxjs';
+import { isNullOrUndefined } from 'util';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpInterceptorsService {
+  constructor(private configService: ConfigService) {}
+
+  intercept( req: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
+    const jwt: JWT = this.configService.getJWT;
+    if (!isNullOrUndefined(jwt)) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${jwt.access_token}`
+        }
+      });
+    }
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.configService.refreshToken();
+        }
+        return throwError(error);
+      })
+    );
+  }
+}
+
+export const HttpInterceptorProvider = [
+  { provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorsService, multi: true }
+];
