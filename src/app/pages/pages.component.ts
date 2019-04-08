a17db4b0061119ca455789959e9a2612372872af
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-04-08 10:49:20 -0500

    Modified comments on pages.component.ts

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -16,9 +16,9 @@ export class PagesComponent implements OnInit {
     private router: Router,
     private idle: Idle,
   ) {
-    // sets an idle timeout of 5 seconds, for testing purposes.
+    // sets an idle timeout of 1 seconds, for testing purposes.
     idle.setIdle(1);
-    // sets a timeout period of 5 seconds. after 10 seconds of inactivity, the user will be considered timed out.
+    // sets a timeout period of 899 seconds. after 10 seconds of inactivity, the user will be considered timed out.
     idle.setTimeout(899);
     // sets the default interrupts, in this case, things like clicks, scrolls, touches to the document
     idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
