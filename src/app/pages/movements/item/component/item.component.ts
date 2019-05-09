6f724433a9b20092128d3e0a7f875d397a479afc
Alan Vargas <alan.vargas@finerio.mx>
2019-05-09 09:22:48 -0500

    bug with icons fixed

--- src/app/pages/movements/item/component/item.component.ts
+++ src/app/pages/movements/item/component/item.component.ts
@@ -1,58 +1,51 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AccountService } from '@services/account/account.service';
 import { DateApiService } from '@services/date-api/date-api.service';
 import { Movement } from '@interfaces/movement.interface';
 import { Category } from '@interfaces/category.interface';
-import { isNullOrUndefined } from 'util';
+import { reject } from 'q';
 
 @Component({
 	selector: 'app-item',
 	templateUrl: './item.component.html',
 	styleUrls: [ './item.component.css' ]
 })
-export class ItemComponent implements OnInit, OnDestroy {
+export class ItemComponent implements OnInit {
 	@Input() movement: Movement;
 	@Input() categoryList: Category[];
 
 	@Output() movementEdited: EventEmitter<Movement>;
 	@Output() valueCategoryColor: EventEmitter<string>;
 
-	traditionalImg: boolean;
+	traditionalImgSrc: string;
+	manualAccountImgSrc: string;
+
 	accountWithOutDefaults: string;
 	amountEdit: boolean;
 
 	constructor(private dateApi: DateApiService, private accountService: AccountService) {
-		this.traditionalImg = true;
-		this.accountWithOutDefaults = '';
 		this.movementEdited = new EventEmitter();
 		this.valueCategoryColor = new EventEmitter();
 		this.amountEdit = true;
 	}
 
 	ngOnInit() {
-		this.traditionalImg = this.traditionalImgProcess();
 		this.accountWithOutDefaults = this.accountService.getManualAccountNatureWithOutDefaults(
-			this.movement.account.nature
+			this.movement.account.type
 		);
+		this.manualAccountImgSrc = `assets/media/img/manual_account/${this.accountWithOutDefaults}.svg`;
+		this.traditionalImgSrc = `https://cdn.finerio.mx/banks/${this.movement.account.institution.code}_shield.png`;
 		this.formatMovementDate();
 		this.editAmountAvailable();
 	}
 
-	ngOnDestroy() {
-		this.traditionalImg = true;
-		this.accountWithOutDefaults = '';
+	onErrorFunc(type: string) {
+		this.accountWithOutDefaults = this.accountService.getManualAccountNatureWithOutDefaults(type);
+		this.manualAccountImgSrc = `assets/media/img/manual_account/${this.accountWithOutDefaults}.svg`;
 	}
 
 	editAmountAvailable() {
 		this.amountEdit = this.movement.account.institution.code == 'DINERIO' ? true : false;
-	}
-
-	traditionalImgProcess(): boolean {
-		if (!isNullOrUndefined(this.movement.account.nature)) {
-			return this.movement.account.nature.includes('ma_') ? false : true;
-		} else {
-			return true;
-		}
 	}
 
 	formatMovementDate() {
