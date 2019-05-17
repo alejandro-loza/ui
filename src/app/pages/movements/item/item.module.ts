6b3cd6e388398c96ea284a8ad032cb411a5123db
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-05-16 23:09:56 -0500

    Erased dateModule

--- src/app/pages/movements/item/item.module.ts
+++ src/app/pages/movements/item/item.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '@shared/shared.module';
 
-import { DateModule } from '../date/date.module';
 import { ItemComponent } from './component/item.component';
 import { ManualAccountEditerModule } from '../manual-account-editer/manual-account-editer.module';
 import { ConceptTypeMovementModule } from '../concept-type-movement/concept-type-movement.module';
 
 @NgModule({
-	declarations: [ ItemComponent ],
-	imports: [ SharedModule, DateModule, ConceptTypeMovementModule, ManualAccountEditerModule ],
-	exports: [ ItemComponent ],
-	providers: []
+  declarations: [ ItemComponent ],
+  imports: [
+    SharedModule,
+    ConceptTypeMovementModule,
+    ManualAccountEditerModule ],
+  exports: [ ItemComponent ],
+  providers: []
 })
 export class ItemModule {}
