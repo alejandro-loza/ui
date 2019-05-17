56fff3ea5b69d80125b8ebc8b1358af1ce971a75
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-05-16 22:53:30 -0500

    Erased typesModule

--- src/app/pages/movements/new-movement/new-movement.module.ts
+++ src/app/pages/movements/new-movement/new-movement.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '@shared/shared.module';
 import { BackButtonModule } from '@components/back-button/back-button.module';
-import { TypeModule } from '../item-detail/type/type.module';
 
 import { DateApiService } from '@services/date-api/date-api.service';
 import { MovementsService } from '@services/movements/movements.service';
@@ -15,9 +14,9 @@ import { NewMovementComponent } from './component/new-movement.component';
 import { DateModule } from '../date/date.module';
 
 @NgModule({
-	declarations: [ NewMovementComponent ],
-	imports: [ NEW_MOVEMENT_ROUTER, SharedModule, DateModule, TypeModule, BackButtonModule, UserManualAccountsModule ],
-	exports: [ NewMovementComponent ],
-	providers: [ DateApiService, MovementsService, ToastService ]
+  declarations: [ NewMovementComponent ],
+  imports: [ NEW_MOVEMENT_ROUTER, SharedModule, DateModule, BackButtonModule, UserManualAccountsModule ],
+  exports: [ NewMovementComponent ],
+  providers: [ DateApiService, MovementsService, ToastService ]
 })
 export class NewMovementModule {}
