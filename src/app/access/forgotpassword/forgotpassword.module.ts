542c28095dd69ce5b377ea2405713db71e82c69c
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-05-16 23:14:31 -0500

    Added logo, and modified styleguides

--- src/app/access/forgotpassword/forgotpassword.module.ts
+++ src/app/access/forgotpassword/forgotpassword.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '@shared/shared.module';
+import {SvgIconsModule} from '@app/svg/svg-icons.module';
 
 // ROUTE
 import { FORGOT_PASS_ROUTES } from './forgotpassword.route';
@@ -14,6 +15,7 @@ import { ForgotpasswordComponent } from './components/forgotpassword.component';
   imports: [
     SharedModule,
     ReactiveFormsModule,
+    SvgIconsModule,
     FORGOT_PASS_ROUTES,
   ],
   declarations: [
