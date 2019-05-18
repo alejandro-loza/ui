1f073a2e82a523d3724a7912bd5700fb2bf51e54
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-05-18 18:00:45 -0500

    Modified interfaces

--- src/app/interfaces/movement.interface.ts
+++ src/app/interfaces/movement.interface.ts
@@ -2,22 +2,22 @@ import { AccountInterface } from './account.interfaces';
 import { Concept } from './concept.interface';
 
 export interface Movement {
-  account: AccountInterface;
-  amount: number;
-  balance: number;
-  concepts: Concept[];
+  account?: AccountInterface;
+  amount?: number;
+  balance?: number;
+  concepts?: Concept[];
   customAmount?: number;
-  customDate: Date;
-  customDescription: string;
-  date: Date;
-  dateCreated: Date;
+  customDate?: Date;
+  customDescription?: string;
+  date?: Date;
+  dateCreated?: Date;
   dateDeleted?: Date;
-  description: string;
-  duplicated: boolean;
+  description?: string;
+  duplicated?: boolean;
   formatDate?: string;
-  hasConcepts: boolean;
-  readonly id: string;
-  lastUpdated: string;
+  hasConcepts?: boolean;
+  readonly id?: string;
+  lastUpdated?: string;
   inBalance?: boolean;
-  type: string;
+  type?: string;
 }
