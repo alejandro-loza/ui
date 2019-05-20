fc197fb6a639cb9c3b297bbc1defeda485764691
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-05-20 13:19:06 -0500

    Modified docs for movement's interface

--- src/app/interfaces/movement.interface.ts
+++ src/app/interfaces/movement.interface.ts
@@ -1,6 +1,22 @@
 import { AccountInterface } from './account.interfaces';
 import { Concept } from './concept.interface';
 
+
+/**
+ * @interface Movement Estos son los datos que lleva un movimiento cuando se hace una petición al servidor, para
+ * obtener la lista de movimientos. Así mismo esta interfaz se utiliza para la creación de un movimientos.
+ *
+ * @param amount - La propiedad es de tipo number y es el monto que lleva el movimiento
+ * @param balance - La propiedad es legacy code
+ * @param date - La propiedad es de tipo String, y es la fecha que se le asigno al movimiento, este debe ser de fomato ISO-8601
+ * y el formato es: YYYY-MM-DDTHH:mm:ss.sssZ
+ * @param description - La propiedad es de tipo string
+ * @param duplicated - La propiedad es de tipo boolean y nos dirá si queremos que se vea reflejado en los grafos del Dashboard
+ * @param type - La propiedad es de tipo string y también es una constante que solo puede llevar dos valores
+ *               CHARGE o DEPOSIT, cualquier otro valor mandará un error status 22 - Unprocessable Entity
+ */
+
+
 export interface Movement {
   account?: AccountInterface;
   amount?: number;
