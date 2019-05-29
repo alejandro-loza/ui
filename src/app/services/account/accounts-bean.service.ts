aaa9920020cbc426e30a890529c06ddca39ab100
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-05-29 00:12:12 -0500

    Modified spaces by IDE

--- src/app/services/account/accounts-bean.service.ts
+++ src/app/services/account/accounts-bean.service.ts
@@ -2,39 +2,48 @@ import { Injectable, Output, EventEmitter } from '@angular/core';
 import { AccountInterface } from '@app/interfaces/account.interfaces';
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
 export class AccountsBeanService {
-	@Output() changeManualAccountOnMovements: EventEmitter<boolean>;
-	private manualAccount: AccountInterface;
-	private manualAccounts: AccountInterface[];
-	private manualAccountToEdit: AccountInterface;
+  @Output() changeManualAccountOnMovements: EventEmitter<boolean>;
+  private accounts: AccountInterface[];
+  private manualAccount: AccountInterface;
+  private manualAccounts: AccountInterface[];
+  private manualAccountToEdit: AccountInterface;
 
-	constructor() {
-		this.changeManualAccountOnMovements = new EventEmitter();
-	}
+  constructor() {
+    this.changeManualAccountOnMovements = new EventEmitter();
+  }
 
-	set setMaualAccountToMovementsEditer(data: AccountInterface) {
-		this.manualAccount = data;
-	}
+  set setMaualAccountToMovementsEditer(data: AccountInterface) {
+    this.manualAccount = data;
+  }
 
-	get getMaualAccountToMovementsEditer(): AccountInterface {
-		return this.manualAccount;
-	}
+  get getMaualAccountToMovementsEditer(): AccountInterface {
+    return this.manualAccount;
+  }
 
-	set setManualAccountToEdit(data: AccountInterface) {
-		this.manualAccountToEdit = data;
-	}
+  set setManualAccountToEdit(data: AccountInterface) {
+    this.manualAccountToEdit = data;
+  }
 
-	get getManualAccountToEdit(): AccountInterface {
-		return this.manualAccountToEdit;
-	}
+  get getManualAccountToEdit(): AccountInterface {
+    return this.manualAccountToEdit;
+  }
 
-	set setManualAccounts(data: AccountInterface[]) {
-		this.manualAccounts = data;
-	}
+  set setManualAccounts(data: AccountInterface[]) {
+    this.manualAccounts = data;
+  }
 
-	get getManualAccounts(): AccountInterface[] {
-		return this.manualAccounts;
-	}
+  get getManualAccounts(): AccountInterface[] {
+    return this.manualAccounts;
+  }
+
+  set setAccounts(data: AccountInterface[]) {
+    this.accounts = data;
+  }
+
+  get getAccounts(): AccountInterface[] {
+    return this.accounts;
+  }
 }
