27a4d60dab54cc6eae3fd6e709828c1ff7174afc
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-05-29 00:12:30 -0500

    Modified spaces by IDE

--- src/app/services/categories/categories.service.ts
+++ src/app/services/categories/categories.service.ts
@@ -10,61 +10,61 @@ import { WorkshopCategory } from '@app/interfaces/categories/workshopCategory.in
 import { CategoryId } from '@app/interfaces/categories/categoryId.interface';
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
 export class CategoriesService {
-	private category: Category;
-	constructor(
-		private http: HttpClient,
-		private configService: ConfigService,
-		private categoriesBeanService: CategoriesBeanService
-	) {}
+  private category: Category;
+  constructor(
+    private http: HttpClient,
+    private configService: ConfigService,
+    private categoriesBeanService: CategoriesBeanService
+  ) {}
 
-	getCategoriesInfo(): Observable<HttpResponse<Category[]>> {
-		const URL = `${environment.apiUrl}/apiv2/categories`;
-		return this.http
-			.get<Category[]>(URL, {
-				observe: 'response',
-				headers: this.configService.getHeaders
-			})
-			.pipe(
-				map((res) => {
-					this.categoriesBeanService.setCategories(res.body);
-					return res;
-				})
-			);
-	}
+  getCategoriesInfo(): Observable<HttpResponse<Category[]>> {
+    const URL = `${environment.apiUrl}/apiv2/categories`;
+    return this.http
+      .get<Category[]>(URL, {
+        observe: 'response',
+        headers: this.configService.getHeaders
+      })
+      .pipe(
+        map((res) => {
+          this.categoriesBeanService.setCategories(res.body);
+          return res;
+        })
+      );
+  }
 
-	getPreliminarCategory(description: string, income: boolean): Observable<HttpResponse<CategoryId>> {
-		const URL = `${environment.apiUrl}//apiv2/categories/search`;
-		let body: any = { text: description, income: income };
-		return this.http.post<CategoryId>(URL, body, {
-			observe: 'response',
-			headers: this.configService.getHeaders
-		});
-	}
+  getPreliminarCategory(description: string, income: boolean): Observable<HttpResponse<CategoryId>> {
+    const URL = `${environment.apiUrl}//apiv2/categories/search`;
+    let body: any = { text: description, income: income };
+    return this.http.post<CategoryId>(URL, body, {
+      observe: 'response',
+      headers: this.configService.getHeaders
+    });
+  }
 
-	createCategoryOrSubcategory(struct: WorkshopCategory): Observable<HttpResponse<Category>> {
-		const URL = `${environment.backendUrl}/users/${this.configService.getUser.id}/categories`;
-		return this.http.post<Category>(URL, struct, {
-			observe: 'response',
-			headers: this.configService.getHeaders
-		});
-	}
+  createCategoryOrSubcategory(struct: WorkshopCategory): Observable<HttpResponse<Category>> {
+    const URL = `${environment.backendUrl}/users/${this.configService.getUser.id}/categories`;
+    return this.http.post<Category>(URL, struct, {
+      observe: 'response',
+      headers: this.configService.getHeaders
+    });
+  }
 
-	updateCategoryOrSubcategory(category: WorkshopCategory): Observable<HttpResponse<Category>> {
-		const URL = `${environment.backendUrl}/categories/${category.id}`;
-		return this.http.put<Category>(URL, category, {
-			observe: 'response',
-			headers: this.configService.getHeaders
-		});
-	}
+  updateCategoryOrSubcategory(category: WorkshopCategory): Observable<HttpResponse<Category>> {
+    const URL = `${environment.backendUrl}/categories/${category.id}`;
+    return this.http.put<Category>(URL, category, {
+      observe: 'response',
+      headers: this.configService.getHeaders
+    });
+  }
 
-	deleteCategoryOrSubcategory(category: Category): Observable<HttpResponse<Category>> {
-		const URL = `${environment.backendUrl}/categories/${category.id}`;
-		return this.http.delete<Category>(URL, {
-			observe: 'response',
-			headers: this.configService.getHeaders
-		});
-	}
+  deleteCategoryOrSubcategory(category: Category): Observable<HttpResponse<Category>> {
+    const URL = `${environment.backendUrl}/categories/${category.id}`;
+    return this.http.delete<Category>(URL, {
+      observe: 'response',
+      headers: this.configService.getHeaders
+    });
+  }
 }
