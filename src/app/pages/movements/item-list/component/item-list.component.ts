a36ddd011c1daaaf4950430c4663c212f82ed1b1
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-05-31 16:09:36 -0500

    removed variables, and libs

--- src/app/pages/movements/item-list/component/item-list.component.ts
+++ src/app/pages/movements/item-list/component/item-list.component.ts
@@ -2,21 +2,17 @@ import {
   AfterViewInit,
   ChangeDetectionStrategy,
   Component,
-  EventEmitter,
   Input,
-  OnInit,
-  Output,
-  ViewChild
+  OnInit
 } from '@angular/core';
 import {Router} from '@angular/router';
+import {ViewportScroller} from '@angular/common';
 
 import {StatefulMovementsService} from '@services/stateful/movements/stateful-movements.service';
 
 import { Movement } from '@interfaces/movement.interface';
+import {ScrollPositionService} from '@services/scroll-position/scroll-position.service';
 
-import { CdkVirtualScrollViewport, ScrollDispatcher } from '@angular/cdk/scrolling';
-
-import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-item-list',
@@ -27,43 +23,29 @@ import {filter} from 'rxjs/operators';
 export class ItemListComponent implements OnInit, AfterViewInit {
   @Input() movementList: Movement[];
   @Input() spinnerBoolean: boolean;
-  @Input() getMoreMovements: boolean;
-
-  @Output() getMoreMovementsChange: EventEmitter<boolean>;
-  @Output() movementListChange: EventEmitter<Movement[]>;
-  @Output() refreshMovementList: EventEmitter<boolean>;
-
-  @ViewChild(CdkVirtualScrollViewport) scrollVirtual: CdkVirtualScrollViewport;
 
   constructor(
-    private scrollDispatcher: ScrollDispatcher,
     private statefulMovementsService: StatefulMovementsService,
-    private router: Router
-  ) {
-    this.getMoreMovements = false;
-
-    this.getMoreMovementsChange = new EventEmitter();
-    this.movementListChange = new EventEmitter();
-    this.refreshMovementList = new EventEmitter();
-  }
+    private router: Router,
+    private scrollPosition: ScrollPositionService,
+    public viewportScroller: ViewportScroller
+  ) { }
 
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-    if (this.scrollVirtual) {
-      this.scrollDispatcher.scrolled().pipe(
-        filter(() => {
-          if (this.scrollVirtual.getRenderedRange().end === this.scrollVirtual.getDataLength()) { return true; }
-        })
-      ).subscribe(() => this.getMoreMovementsChange.emit(true));
+    if (this.scrollPosition.scrollPositionValue) {
+      this.viewportScroller.scrollToPosition(this.scrollPosition.scrollPositionValue);
+      this.scrollPosition.scrollPositionValue = [0, 0];
     }
   }
 
   trackByFn = (index: number, movement: Movement) => movement.id;
 
-  setMovement(movemment: Movement) {
-    this.statefulMovementsService.setMovement = JSON.parse(JSON.stringify(movemment));
-    this.router.navigateByUrl(`/app/movements/${movemment.id}`);
+  setMovement(movement: Movement) {
+    this.scrollPosition.scrollPositionValue = this.viewportScroller.getScrollPosition();
+    this.statefulMovementsService.setMovement = JSON.parse(JSON.stringify(movement));
+    return this.router.navigateByUrl(`/app/movements/${movement.id}`);
   }
 
 }
