1da0a82a0a895290eda48aba6805be5e8183a259
Alan Vargas <alan.vargas@finerio.mx>
2019-06-04 09:59:12 -0500

    clenaer service called when editing and delete

--- src/app/services/cleaner/cleaner.service.ts
+++ src/app/services/cleaner/cleaner.service.ts
@@ -6,90 +6,99 @@ import { BudgetsBeanService } from '@services/budgets/budgets-bean.service';
 import { CategoriesBeanService } from '@services/categories/categories-bean.service';
 import { EmptyStateService } from '@services/movements/empty-state/empty-state.service';
 import { AccountsBeanService } from '@services/account/accounts-bean.service';
-import {StatefulMovementsService} from "@services/stateful/movements/stateful-movements.service";
+import { StatefulMovementsService } from '@services/stateful/movements/stateful-movements.service';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class CleanerService {
-  constructor(
-    private dashboardBean: DashboardBeanService,
-    private dashboardStatesService: DashboardStatesService,
-    private credentialBeanService: CredentialBeanService,
-    private budgetsBeanService: BudgetsBeanService,
-    private categoriesBeanService: CategoriesBeanService,
-    private emptyStateService: EmptyStateService,
-    private accountsBeanService: AccountsBeanService,
-    private statefulMovementsService: StatefulMovementsService
-  ) {}
+	constructor(
+		private dashboardBean: DashboardBeanService,
+		private dashboardStatesService: DashboardStatesService,
+		private credentialBeanService: CredentialBeanService,
+		private budgetsBeanService: BudgetsBeanService,
+		private categoriesBeanService: CategoriesBeanService,
+		private emptyStateService: EmptyStateService,
+		private accountsBeanService: AccountsBeanService,
+		private statefulMovementsService: StatefulMovementsService
+	) {}
 
-  cleanAllVariables() {
-    // Dashboard Memory
-    this.dashboardBean.setDataStackedBar([]);
-    this.dashboardBean.setDataBalancePieChart([]);
-    this.dashboardBean.setLoadInformation(true);
-    this.dashboardBean.setDataIsReady(false);
-    this.dashboardBean.setDataExpensesTab([]);
-    this.dashboardBean.setDataIncomesBarChart([]);
-    this.dashboardBean.setDataIncomesTab([]);
-    this.dashboardBean.setShowEmptyState(false);
-    // Dashboard States
-    this.dashboardStatesService.setListOfMovementsFromDashboard([]);
-    this.dashboardStatesService.setLoadListFromDashboard(false);
-    this.dashboardStatesService.setIndexOfMonthToShow(0);
-    this.dashboardStatesService.setLoadClickedScreen(false);
-    this.dashboardStatesService.setElementToShowOnClickedScreen({});
-    this.dashboardStatesService.setNumberOfTabToReturn(1);
-    // Credentials Memory
-    this.credentialBeanService.setCredentials([]);
-    this.credentialBeanService.setAccounts([]);
-    this.credentialBeanService.setInstitutions([]);
-    this.credentialBeanService.setLoadInformation(true);
-    this.credentialBeanService.setShowEmptyState(false);
-    // Budgets Memory
-    this.budgetsBeanService.setBudgetToViewDetails(null);
-    this.budgetsBeanService.setBudgets([]);
-    this.budgetsBeanService.setLoadInformation(true);
-    this.budgetsBeanService.setShowEmptyStates(false);
-    this.budgetsBeanService.setCategoryToSharedComponent(null);
-    // Categories memory
-    this.categoriesBeanService.setCategories([]);
-    this.categoriesBeanService.setCategoryToViewDetails(null);
-    this.categoriesBeanService.setSubcategoryToViewDetails(null);
-    // Movements memeory
-    this.emptyStateService.setShowEmptyState(false);
-    // Manual Accounts memory
-    this.accountsBeanService.setManualAccounts = null;
-    this.accountsBeanService.setManualAccountToEdit = null;
-    // Movements stateful service
-    this.statefulMovementsService.setMovements = null;
-    this.statefulMovementsService.setMovement = undefined;
-  }
+	cleanAllVariables() {
+		// Dashboard Memory
+		this.dashboardBean.setDataStackedBar([]);
+		this.dashboardBean.setDataBalancePieChart([]);
+		this.dashboardBean.setLoadInformation(true);
+		this.dashboardBean.setDataIsReady(false);
+		this.dashboardBean.setDataExpensesTab([]);
+		this.dashboardBean.setDataIncomesBarChart([]);
+		this.dashboardBean.setDataIncomesTab([]);
+		this.dashboardBean.setShowEmptyState(false);
+		// Dashboard States
+		this.dashboardStatesService.setListOfMovementsFromDashboard([]);
+		this.dashboardStatesService.setLoadListFromDashboard(false);
+		this.dashboardStatesService.setIndexOfMonthToShow(0);
+		this.dashboardStatesService.setLoadClickedScreen(false);
+		this.dashboardStatesService.setElementToShowOnClickedScreen({});
+		this.dashboardStatesService.setNumberOfTabToReturn(1);
+		// Credentials Memory
+		this.credentialBeanService.setCredentials([]);
+		this.credentialBeanService.setAccounts([]);
+		this.credentialBeanService.setInstitutions([]);
+		this.credentialBeanService.setLoadInformation(true);
+		this.credentialBeanService.setShowEmptyState(false);
+		// Budgets Memory
+		this.budgetsBeanService.setBudgetToViewDetails(null);
+		this.budgetsBeanService.setBudgets([]);
+		this.budgetsBeanService.setLoadInformation(true);
+		this.budgetsBeanService.setShowEmptyStates(false);
+		this.budgetsBeanService.setCategoryToSharedComponent(null);
+		// Categories memory
+		this.categoriesBeanService.setCategories([]);
+		this.categoriesBeanService.setCategoryToViewDetails(null);
+		this.categoriesBeanService.setSubcategoryToViewDetails(null);
+		// Movements memeory
+		this.emptyStateService.setShowEmptyState(false);
+		// Manual Accounts memory
+		this.accountsBeanService.setManualAccounts = null;
+		this.accountsBeanService.setManualAccountToEdit = null;
+		// Movements stateful service
+		this.statefulMovementsService.setMovements = null;
+		this.statefulMovementsService.setMovement = undefined;
+	}
 
-  cleanDashboardVariables() {
-    // Dashboard Memory
-    this.dashboardBean.setDataStackedBar([]);
-    this.dashboardBean.setDataBalancePieChart([]);
-    this.dashboardBean.setLoadInformation(true);
-    this.dashboardBean.setDataIsReady(false);
-    this.dashboardBean.setDataExpensesTab([]);
-    this.dashboardBean.setDataIncomesBarChart([]);
-    this.dashboardBean.setDataIncomesTab([]);
-    this.dashboardBean.setShowEmptyState(false);
-    this.dashboardStatesService.setListOfMovementsFromDashboard([]);
-    this.dashboardStatesService.setLoadListFromDashboard(false);
-    this.dashboardStatesService.setIndexOfMonthToShow(0);
-    this.dashboardStatesService.setLoadClickedScreen(false);
-    this.dashboardStatesService.setElementToShowOnClickedScreen({});
-    this.dashboardStatesService.setNumberOfTabToReturn(1);
-  }
+	cleanCredentialsVariables() {
+		// Credentials Memory
+		this.credentialBeanService.setCredentials([]);
+		this.credentialBeanService.setAccounts([]);
+		this.credentialBeanService.setInstitutions([]);
+		this.credentialBeanService.setLoadInformation(true);
+		this.credentialBeanService.setShowEmptyState(false);
+	}
 
-  cleanBudgetsVariables() {
-    // Budgets Memory
-    this.budgetsBeanService.setBudgetToViewDetails(null);
-    this.budgetsBeanService.setBudgets([]);
-    this.budgetsBeanService.setLoadInformation(true);
-    this.budgetsBeanService.setShowEmptyStates(false);
-    this.budgetsBeanService.setCategoryToSharedComponent(null);
-  }
+	cleanDashboardVariables() {
+		// Dashboard Memory
+		this.dashboardBean.setDataStackedBar([]);
+		this.dashboardBean.setDataBalancePieChart([]);
+		this.dashboardBean.setLoadInformation(true);
+		this.dashboardBean.setDataIsReady(false);
+		this.dashboardBean.setDataExpensesTab([]);
+		this.dashboardBean.setDataIncomesBarChart([]);
+		this.dashboardBean.setDataIncomesTab([]);
+		this.dashboardBean.setShowEmptyState(false);
+		this.dashboardStatesService.setListOfMovementsFromDashboard([]);
+		this.dashboardStatesService.setLoadListFromDashboard(false);
+		this.dashboardStatesService.setIndexOfMonthToShow(0);
+		this.dashboardStatesService.setLoadClickedScreen(false);
+		this.dashboardStatesService.setElementToShowOnClickedScreen({});
+		this.dashboardStatesService.setNumberOfTabToReturn(1);
+	}
+
+	cleanBudgetsVariables() {
+		// Budgets Memory
+		this.budgetsBeanService.setBudgetToViewDetails(null);
+		this.budgetsBeanService.setBudgets([]);
+		this.budgetsBeanService.setLoadInformation(true);
+		this.budgetsBeanService.setShowEmptyStates(false);
+		this.budgetsBeanService.setCategoryToSharedComponent(null);
+	}
 }
