37c9df5b0d325b8a38a2f0a1696d46cd98dbe1d3
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-06-12 12:31:59 -0500

    Added animation for welcome

--- src/app/access/welcome/welcome.module.ts
+++ src/app/access/welcome/welcome.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from             '@angular/core';
 import { SharedModule } from         '@shared/shared.module';
-import {SvgIconsModule} from         '@app/svg/svg-icons.module';
 
 import { WelcomeComponent } from     './components/welcome.component';
 import { WelcomeRoutes } from        './welcome.routes';
+import {WelcomeAnimationComponent} from '@components/welcome-animation/welcome-animation.component';
 
 @NgModule({
   imports: [
     SharedModule,
-    SvgIconsModule,
     WelcomeRoutes,
   ],
   declarations: [
     WelcomeComponent,
+    WelcomeAnimationComponent,
   ],
   providers: [ ]
 })
