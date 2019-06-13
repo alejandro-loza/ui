17b3f501a43fdd9c3f45a0bf875bffcb493f69a8
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-06-13 16:09:59 -0500

    Created component for each image on svg

---
+++ src/app/access/security-help/svg/credit-card-security/credit-card-security.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-credit-card-security',
+  templateUrl: './credit-card-security.component.html',
+  styleUrls: ['./credit-card-security.component.css']
+})
+export class CreditCardSecurityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/access/security-help/svg/ribbon-security/ribbon-security.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ribbon-security',
+  templateUrl: './ribbon-security.component.html',
+  styleUrls: ['./ribbon-security.component.css']
+})
+export class RibbonSecurityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
