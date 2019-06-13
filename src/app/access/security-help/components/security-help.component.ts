bd2ee6d040ab791098e57d3989f703a80baaf49a
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-06-13 16:09:37 -0500

    Redesigned layout

--- src/app/access/security-help/components/security-help.component.ts
+++ src/app/access/security-help/components/security-help.component.ts
@@ -1,19 +1,12 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import M from 'materialize-css/dist/js/materialize';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-security-help',
   templateUrl: './security-help.component.html',
   styleUrls: ['./security-help.component.css']
 })
-export class SecurityHelpComponent implements OnInit, AfterViewInit {
-  @ViewChild('slider') sliderElement: ElementRef;
+export class SecurityHelpComponent implements OnInit {
   constructor() { }
 
   ngOnInit() { }
-
-  ngAfterViewInit(): void {
-    const sliderInstance = new M.Slider(this.sliderElement.nativeElement, { interval: 100000 });
-    sliderInstance.pause();
-  }
 }
--- src/app/access/security-help/svg/security-svg.module.ts
+++ src/app/access/security-help/svg/security-svg.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 import {CobrosComponent} from './cobros/cobros.component';
 import {ComprasComponent} from './compras/compras.component';
 import {TransferenciaComponent} from './transferencia/transferencia.component';
+import { TitleSecurityHelpComponent } from './title-security-help/title-security-help.component';
+import { CreditCardSecurityComponent } from './credit-card-security/credit-card-security.component';
+import { RibbonSecurityComponent } from './ribbon-security/ribbon-security.component';
 
 @NgModule({
   declarations: [
     CobrosComponent,
     ComprasComponent,
     TransferenciaComponent,
+    TitleSecurityHelpComponent,
+    CreditCardSecurityComponent,
+    RibbonSecurityComponent,
   ],
   imports: [
     CommonModule
@@ -17,6 +23,9 @@ import {TransferenciaComponent} from './transferencia/transferencia.component';
     CobrosComponent,
     ComprasComponent,
     TransferenciaComponent,
+    TitleSecurityHelpComponent,
+    CreditCardSecurityComponent,
+    RibbonSecurityComponent,
   ]
 })
 export class SecuritySvgModule { }
