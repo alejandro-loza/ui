5947b815a51dd5d0f13fead99da021b233d893f9
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-06-18 15:00:06 -0500

    Added two paths for return to login page

--- src/app/access/security-help/security-help.route.ts
+++ src/app/access/security-help/security-help.route.ts
@@ -4,7 +4,8 @@ import { ModuleWithProviders } from           '@angular/core';
 import {SecurityHelpComponent} from           './components/security-help.component';
 
 const SecurityHelpRouting: Routes = [
-  { path: '', component: SecurityHelpComponent }
+  { path: '', component: SecurityHelpComponent },
+  { path: '**', redirectTo: '/access/login' }
 ];
 
 export const SecurityHelpRoutes: ModuleWithProviders = RouterModule.forChild( SecurityHelpRouting );
