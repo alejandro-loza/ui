98f715c7290d98397e3bfc42b52acd5c072b2328
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-06-18 16:07:49 -0500

    Added first.step child module

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -2,32 +2,37 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth/auth.guard';
 
 const AppRouting: Routes = [
-	{
-		path: 'access',
-		loadChildren: './access/access.module#AccessModule'
-	},
-	{
-		path: 'app',
-		canActivate: [ AuthGuard ],
-		loadChildren: './pages/pages.module#PagesModule'
-	},
-	{
-		path: 'invitation/:code',
-		loadChildren: './referals/referals.module#ReferalsModule'
-	},
-	{
-		path: 'invitation-success',
-		loadChildren: './referals/success/success.module#SuccessModule'
-	},
-	{
-		path: '',
-		pathMatch: 'full',
-		redirectTo: '/access/login'
-	},
-	{
-		path: '**',
-		redirectTo: '/access/login'
-	}
+  {
+    path: 'access',
+    loadChildren: './access/access.module#AccessModule'
+  },
+  {
+    path: 'app',
+    canActivate: [ AuthGuard ],
+    loadChildren: './pages/pages.module#PagesModule'
+  },
+  {
+    path: 'first-step',
+    canActivate: [ AuthGuard ],
+    loadChildren: './first-step/first-step.module#FirstStepModule'
+  },
+  {
+    path: 'invitation/:code',
+    loadChildren: './referals/referals.module#ReferalsModule'
+  },
+  {
+    path: 'invitation-success',
+    loadChildren: './referals/success/success.module#SuccessModule'
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/access/login'
+  },
+  {
+    path: '**',
+    redirectTo: '/access/login'
+  }
 ];
 
 export const AppRoutes = RouterModule.forRoot(AppRouting);
