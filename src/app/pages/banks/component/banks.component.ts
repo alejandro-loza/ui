27e7ae281f03c9e31869f0856c8b1ac174fd09c5
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-06-20 12:55:32 -0500

    Spaced from IDE

--- src/app/pages/banks/component/banks.component.ts
+++ src/app/pages/banks/component/banks.component.ts
@@ -5,43 +5,43 @@ import { InstitutionInterface } from '@interfaces/institution.interface';
 import * as M from 'materialize-css/dist/js/materialize';
 
 @Component({
-	selector: 'app-banks',
-	templateUrl: './banks.component.html',
-	styleUrls: [ './banks.component.css' ]
+  selector: 'app-banks',
+  templateUrl: './banks.component.html',
+  styleUrls: [ './banks.component.css' ]
 })
 export class BanksComponent implements OnInit {
-	institutions: InstitutionInterface[];
-	@ViewChild('modal') elModal: ElementRef;
+  institutions: InstitutionInterface[];
+  @ViewChild('modal') elModal: ElementRef;
 
-	constructor(private intitutionService: InstitutionService, private credentialBeanService: CredentialBeanService) {
-		this.institutions = [];
-	}
+  constructor(private intitutionService: InstitutionService, private credentialBeanService: CredentialBeanService) {
+    this.institutions = [];
+  }
 
-	ngOnInit() {
-		this.getInstitutions();
-	}
+  ngOnInit() {
+    this.getInstitutions();
+  }
 
-	ngAfterViewInit() {
-		const modal = new M.Modal(this.elModal.nativeElement);
-	}
+  ngAfterViewInit() {
+    const modal = new M.Modal(this.elModal.nativeElement);
+  }
 
-	openModal() {
-		const instanceModal = M.Modal.getInstance(this.elModal.nativeElement);
-		instanceModal.open();
-	}
+  openModal() {
+    const instanceModal = M.Modal.getInstance(this.elModal.nativeElement);
+    instanceModal.open();
+  }
 
-	getInstitutions() {
-		if (this.credentialBeanService.getInstitutions().length == 0) {
-			this.intitutionService.getAllInstitutions().subscribe((res) => {
-				res.body.data.forEach((element: InstitutionInterface) => {
-					if (element.code !== 'DINERIO') {
-						this.institutions.push(element);
-					}
-				});
-				this.credentialBeanService.setInstitutions(this.institutions);
-			});
-		} else {
-			this.institutions = this.credentialBeanService.getInstitutions();
-		}
-	}
+  getInstitutions() {
+    if (this.credentialBeanService.getInstitutions().length == 0) {
+      this.intitutionService.getAllInstitutions().subscribe((res) => {
+        res.body.data.forEach((element: InstitutionInterface) => {
+          if (element.code !== 'DINERIO') {
+            this.institutions.push(element);
+          }
+        });
+        this.credentialBeanService.setInstitutions(this.institutions);
+      });
+    } else {
+      this.institutions = this.credentialBeanService.getInstitutions();
+    }
+  }
 }
