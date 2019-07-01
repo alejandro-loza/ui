b4d8f58e0b9fa18188e09cedcb562efb713b57a7
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-07-01 11:34:58 -0500

    Removed ngDestroy for subscriptions

--- src/app/access/welcome/components/welcome.component.ts
+++ src/app/access/welcome/components/welcome.component.ts
@@ -9,16 +9,15 @@ import {ConfigService} from '@services/config/config.service';
 import {CredentialService} from '@services/credentials/credential.service';
 import {ProcessingCredentialsService} from '@services/credentials/background-process/processing-credentials.service';
 import {Subscription} from 'rxjs';
+import {isNullOrUndefined} from 'util';
 
 @Component({
-	selector: 'app-welcome',
-	templateUrl: './welcome.component.html',
-	styleUrls: [ './welcome.component.css' ]
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: [ './welcome.component.css' ]
 })
 export class WelcomeComponent implements OnInit, OnDestroy {
-  personalInfoUserSubscription: Subscription;
   credentialSubscription: Subscription;
-  accountSubscription: Subscription;
   constructor(
     private accountService: AccountService,
     private authService: AuthService,
@@ -32,39 +31,36 @@ export class WelcomeComponent implements OnInit, OnDestroy {
     private signupService: SignupService
   ) {}
 
-	ngOnInit() {
-		this.personalInfoUser();
-	}
-
-  ngOnDestroy(): void {
-    this.credentialSubscription.unsubscribe();
-    this.accountSubscription.unsubscribe();
-    this.personalInfoUserSubscription.unsubscribe();
+  ngOnInit() {
+    this.personalInfoUser();
   }
-	personalInfoUser() {
-		this.authService.personalInfo().subscribe(
-			(res) => {
-				this.mixpanelEvent(res.body.email);
-				if (res.body.accountLocked === true) {
-					this.toastService.setCode = 401;
-					this.toastService.setMessage =
-						'Tu cuenta fue bloqueada, por favor <br> ponte en contacto con nosotros';
-					this.toastService.toastGeneral();
-				}
-			},
-			(err) => {
-				this.toastService.setCode = err.status;
-				this.toastService.setMessage = 'Ocurrió un error al obtener tus datos';
-				this.toastService.toastGeneral();
-				return this.router.navigate([ 'access/login' ]);
-			},
-			() => {
-        this.mixpanelEvent();
+
+  ngOnDestroy(): void { }
+
+  personalInfoUser() {
+    this.authService.personalInfo().subscribe(
+      (res) => {
+        this.mixpanelEvent(res.body.email);
+        if (res.body.accountLocked === true) {
+          this.toastService.setCode = 401;
+          this.toastService.setMessage =
+            'Tu cuenta fue bloqueada, por favor <br> ponte en contacto con nosotros';
+          this.toastService.toastGeneral();
+          return this.router.navigate([ 'access/login' ]);
+        }
+      },
+      (err) => {
+        this.toastService.setCode = err.status;
+        this.toastService.setMessage = 'Ocurrió un error al obtener tus datos';
+        this.toastService.toastGeneral();
+        return this.router.navigate([ '/access', 'login' ]);
+      },
+      () => {
         this.getCredentials();
-				this.getAccount();
-			}
-		);
-	}
+        this.getAccount();
+      }
+    );
+  }
 
   getCredentials() {
     this.credentialSubscription = this.credentialService.getAllCredentials().subscribe(
@@ -74,41 +70,38 @@ export class WelcomeComponent implements OnInit, OnDestroy {
     );
   }
 
-	getAccount() {
-		this.accountService.getAccounts().subscribe(
-			(res) => {
-				setTimeout(() => {
-					if (this.configService.getUser.name && res.body.size > 1) {
-						return this.router.navigate([ '/app', 'dashboard' ]);
-					} else if (!this.configService.getUser.name) {
-						return this.router.navigate([ '/first-step', 'username' ]);
-					} else {
-						return this.router.navigate([ '/app', 'banks' ]);
-					}
-				}, 2000);
-			},
-			(error) => {
-				return this.router.navigate([ 'access/login' ]);
-			}
-		);
-	}
+  getAccount() {
+    this.accountService.getAccounts().subscribe(
+      (res) => {
+        setTimeout(() => {
+          if (this.configService.getUser.name && res.body.size > 1) {
+            return this.router.navigate([ '/app', 'dashboard' ]);
+          } else if (!this.configService.getUser.name) {
+            return this.router.navigate([ '/first-step', 'username' ]);
+          } else {
+            return this.router.navigate([ '/app', 'banks' ]);
+          }
+        }, 2000);
+      }
+    );
+  }
 
-	mixpanelEvent(email: string) {
-		// Facebook Process
-		if (this.mixpanelService.getFacebookSuccess) {
-			if (this.signupService.getFacebookSignup) {
-				this.mixpanelService.setSignupPeopleProperties(email, new Date());
-				this.mixpanelService.setTrackEvent('Sign up', { from: 'Facebook' });
-			} else if (this.signupService.getFacebookLogin) {
-				this.mixpanelService.setTrackEvent('Log in', { from: 'Facebook' });
-			}
-		} else if (!this.mixpanelService.getFacebookSuccess && !this.signupService.getComesFromSignup) {
-			this.mixpanelService.setTrackEvent('Log in', { from: 'Web' });
-		}
+  mixpanelEvent(email: string) {
+    // Facebook Process
+    if (this.mixpanelService.getFacebookSuccess) {
+      if (this.signupService.getFacebookSignup) {
+        this.mixpanelService.setSignupPeopleProperties(email, new Date());
+        this.mixpanelService.setTrackEvent('Sign up', { from: 'Facebook' });
+      } else if (this.signupService.getFacebookLogin) {
+        this.mixpanelService.setTrackEvent('Log in', { from: 'Facebook' });
+      }
+    } else if (!this.mixpanelService.getFacebookSuccess && !this.signupService.getComesFromSignup) {
+      this.mixpanelService.setTrackEvent('Log in', { from: 'Web' });
+    }
 
-		// En signup ya mando evento
-		this.mixpanelService.setIdentify();
-		this.mixpanelService.setSuperProperties();
-		this.mixpanelService.setPeopleProperties();
-	}
+    // En signup ya mando evento
+    this.mixpanelService.setIdentify();
+    this.mixpanelService.setSuperProperties();
+    this.mixpanelService.setPeopleProperties();
+  }
 }
