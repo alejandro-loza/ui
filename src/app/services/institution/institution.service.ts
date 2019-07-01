ef7bdfaaaf5f1e8b14ea052bee15f448981cde6f
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-07-01 13:03:55 -0500

    Added stateful service

--- src/app/services/institution/institution.service.ts
+++ src/app/services/institution/institution.service.ts
@@ -4,23 +4,34 @@ import {HttpClient, HttpResponse} from '@angular/common/http';
 import {environment} from '@env/environment';
 
 import {ConfigService} from '@services/config/config.service';
+import {StatefulInstitutionsService} from '@stateful/institutions/stateful-institutions.service';
 
 import {InstitutionInterface} from '@interfaces/institution.interface';
 import {Response} from '@interfaces/response.interface';
 
 import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class InstitutionService {
-  constructor(private http: HttpClient, private configService: ConfigService) {}
+  constructor(
+    private httpClient: HttpClient,
+    private configService: ConfigService,
+    private statefulInstitutionsService: StatefulInstitutionsService
+  ) {}
 
   getAllInstitutions(): Observable<HttpResponse<Response<InstitutionInterface>>> {
-    return this.http.get<Response<InstitutionInterface>>(
+    return this.httpClient.get<Response<InstitutionInterface>>(
       `${environment.backendUrl}/institutions`,
       {
         observe: 'response',
         headers: this.configService.getHeaders
       }
+    ).pipe(
+      map(res => {
+        this.statefulInstitutionsService.institutions = res.body.data;
+        return res;
+      })
     );
   }
 }
