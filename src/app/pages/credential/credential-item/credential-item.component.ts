c5caab8c1d10fa6c2ecb5bc0db610000681200c9
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-07-05 00:18:18 -0500

    Modified space by IDE

--- src/app/pages/credential/credential-item/credential-item.component.ts
+++ src/app/pages/credential/credential-item/credential-item.component.ts
@@ -3,52 +3,52 @@ import { CredentialInterface } from '@app/interfaces/credential.interface';
 import { DateApiService } from '@services/date-api/date-api.service';
 
 @Component({
-	selector: 'app-credential-item',
-	templateUrl: './credential-item.component.html',
-	styleUrls: [ './credential-item.component.css' ]
+  selector: 'app-credential-item',
+  templateUrl: './credential-item.component.html',
+  styleUrls: [ './credential-item.component.css' ]
 })
 export class CredentialItemComponent implements OnInit {
-	@Input() credential: CredentialInterface;
-	@Output() syncButtonClicked: EventEmitter<CredentialInterface> = new EventEmitter();
+  @Input() credential: CredentialInterface;
+  @Output() syncButtonClicked: EventEmitter<CredentialInterface> = new EventEmitter();
 
-	iconText: string = '';
-	showButton: boolean = false;
-	constructor(private dateApiService: DateApiService) {}
+  iconText: string = '';
+  showButton: boolean = false;
+  constructor(private dateApiService: DateApiService) {}
 
-	ngOnInit() {
-		this.selectIconToShow();
-		this.showSyncButton();
-	}
+  ngOnInit() {
+    this.selectIconToShow();
+    this.showSyncButton();
+  }
 
-	showSyncButton() {
-		if (this.credential.status == 'INVALID' || this.moreThanEightHours(this.credential)) {
-			this.showButton = true;
-		}
-	}
+  showSyncButton() {
+    if (this.credential.status == 'INVALID' || this.moreThanEightHours(this.credential)) {
+      this.showButton = true;
+    }
+  }
 
-	syncButton() {
-		this.showButton = false;
-		this.syncButtonClicked.emit(this.credential);
-	}
+  syncButton() {
+    this.showButton = false;
+    this.syncButtonClicked.emit(this.credential);
+  }
 
-	moreThanEightHours(credential: CredentialInterface): boolean {
-		let currentMoment = new Date();
-		let dateObj = this.dateApiService.formatDateForAllBrowsers(this.credential.lastUpdated);
-		let diff = (currentMoment.getTime() - dateObj.getTime()) / (1000 * 60 * 60);
-		if (diff >= 8) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+  moreThanEightHours(credential: CredentialInterface): boolean {
+    let currentMoment = new Date();
+    let dateObj = this.dateApiService.formatDateForAllBrowsers(this.credential.lastUpdated);
+    let diff = (currentMoment.getTime() - dateObj.getTime()) / (1000 * 60 * 60);
+    if (diff >= 8) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	selectIconToShow() {
-		if (this.credential.status == 'ACTIVE') {
-			this.iconText = 'verified_user';
-		} else if (this.credential.status == 'INVALID') {
-			this.iconText = 'report_problem';
-		} else {
-			this.iconText = 'sync';
-		}
-	}
+  selectIconToShow() {
+    if (this.credential.status == 'ACTIVE') {
+      this.iconText = 'verified_user';
+    } else if (this.credential.status == 'INVALID') {
+      this.iconText = 'report_problem';
+    } else {
+      this.iconText = 'sync';
+    }
+  }
 }
