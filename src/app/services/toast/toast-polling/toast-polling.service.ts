00a245b843c80ec406983b9a0e10d9998613cf01
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-07-09 12:16:00 -0500

    Created toast polling's service

---
+++ src/app/services/toast/toast-polling/toast-polling.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import {CredentialInterface} from '@interfaces/credentials/credential.interface';
+import {Subscription} from 'rxjs';
+import {ModalTokenComponent} from '@components/modal-token/component/modal-token.component';
+import {TrackingCredentialService} from '@services/credentials/tracking-credential/tracking-credential.service';
+import {ToastService} from '@services/toast/toast.service';
+import {CleanerService} from '@services/cleaner/cleaner.service';
+import {AccountService} from '@services/account/account.service';
+import {MatDialog} from '@angular/material';
+import {InteractiveFieldService} from '@services/interactive-field/interactive-field.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastPollingService {
+
+  constructor(
+    private accountService: AccountService,
+    private cleanerService: CleanerService,
+    private interactiveFieldsService: InteractiveFieldService,
+    private matDialog: MatDialog,
+    private trackingCredentialService: TrackingCredentialService,
+    private toastService: ToastService,
+  ) { }
+
+  showToast( credential: CredentialInterface, subscription: Subscription ): boolean {
+
+    this.toastService.setCode = 200;
+
+    if ( credential.status === 'ACTIVE' ) {
+
+      this.cleanerService.cleanBudgetsVariables();
+      this.cleanerService.cleanDashboardVariables();
+      this.cleanerService.cleanMovements();
+
+      this.accountService.getAccounts().subscribe();
+
+      this.toastService.setMessage = `Tu cuenta de ${ credential.institution.name },<br>ha sido sincronizada`;
+
+      subscription.unsubscribe();
+
+    } else if ( credential.status === 'TOKEN' ) {
+
+      this.interactiveFieldsService.getInteractiveField(credential).subscribe();
+
+      this.matDialog.open(ModalTokenComponent, {
+        width: '750px'
+      });
+
+      this.toastService.setMessage = `¡Necesitamos información extra de tu cuenta bancaria<br>para sincronizarla`;
+
+    } else if ( credential.status === 'INVALID' ) {
+
+      this.toastService.setMessage = `¡Hubo un problema con tu cuenta de ${ credential.institution.name }, revísala!`;
+
+      subscription.unsubscribe();
+
+    }
+
+    this.trackingCredentialService.syncingCredential(credential);
+
+    setTimeout(() => this.toastService.toastGeneral(), 2000);
+
+    return subscription.closed;
+
+  }
+}
