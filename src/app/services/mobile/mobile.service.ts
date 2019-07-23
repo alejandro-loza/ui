3248d1fc27e1c1edaaf6168b8ff393db64ef40a9
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-07-22 22:06:03 -0500

    Added new function on mobileService to know OS mobile

--- src/app/services/mobile/mobile.service.ts
+++ src/app/services/mobile/mobile.service.ts
@@ -21,5 +21,25 @@ export class MobileService {
     })
     (navigator.userAgent || navigator.vendor || (window as any).opera);
     return check;
-  };
+  }
+
+  get mobileOperatingSystem(): string {
+    const userAgent = navigator.userAgent || navigator.vendor || (window as any).opera;
+
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+      return 'Windows Phone';
+    }
+
+    if (/android/i.test(userAgent)) {
+      return 'android';
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream) {
+      return 'iOS';
+    }
+
+    return undefined;
+  }
 }
