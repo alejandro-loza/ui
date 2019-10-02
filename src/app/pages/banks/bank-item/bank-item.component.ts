2af8be552e943fa45997d568a3953503b0b8860c
Aarón Cervantes <aaron.cervantes@finerio.mx>
2019-10-02 11:07:16 -0500

    Moved functions to OauthService

--- src/app/pages/banks/bank-item/bank-item.component.ts
+++ src/app/pages/banks/bank-item/bank-item.component.ts
@@ -1,5 +1,4 @@
 import {AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
-import {MatDialog, MatDialogConfig, MatDialogRef} from '@angular/material/dialog';
 import {Router} from '@angular/router';
 
 import {StatefulInstitutionService} from '@stateful/institution/stateful-institution.service';
@@ -7,12 +6,8 @@ import {OauthService} from '@services/oauth/oauth.service';
 
 import {InstitutionInterface} from '@interfaces/institution.interface';
 
-import {ModalAccountSyncComponent} from '@components/modal-account-sync/component/modal-account-sync.component';
-
 import * as M from 'materialize-css/dist/js/materialize';
 import {OAuthOptionsModel} from '@model/oAuth/oAuth.options.model';
-import {CredentialStatusEnum} from '@interfaces/credentials/oAuth/credential-status.enum';
-import {isNull} from 'util';
 
 @Component({
   selector: 'app-bank-item',
@@ -25,12 +20,9 @@ export class BankItemComponent implements OnInit, AfterViewInit {
 
   @ViewChild('tooltip', {static: false}) elementTooltip: ElementRef;
 
-  private matDialogRef: MatDialogRef<any>;
-
   constructor(
     private route: Router,
     private statefulInstitution: StatefulInstitutionService,
-    private matDialog: MatDialog,
     private oauthService: OauthService,
   ) {}
 
@@ -82,50 +74,10 @@ export class BankItemComponent implements OnInit, AfterViewInit {
 
         this.oauthService.checkOauth( res.body ).subscribe( auxRes => {
 
-          if ( isNull(auxRes) ) {
-            return;
-          }
-
-          if ( auxRes.status !== CredentialStatusEnum.VALIDATE ) {
-            window.close();
-          }
-
-          if ( auxRes.status === CredentialStatusEnum.CREDENTIAL_CONNECT_SUCCESS ) {
-            this.openDialog( auxRes );
-          }
-
-          if ( this.matDialogRef ) {
-
-            console.log(auxRes);
-
-            this.matDialogRef.componentInstance.credentialOauth = {
-              ...this.matDialogRef.componentInstance.credentialOauth,
-              auxRes
-            };
-
-          }
+          this.oauthService.statesOauth( auxRes, window );
 
         });
       });
-  }
-
-  private openDialog( data: any ) {
-    let matDialogConfig: MatDialogConfig<any>;
-
-    matDialogConfig = {
-      autoFocus: true,
-      disableClose: false,
-      closeOnNavigation: false,
-      restoreFocus: true,
-      width: '20%',
-      panelClass: 'custom-dialog',
-      data: data
-    };
-
-    if ( !this.matDialogRef ) {
-      this.matDialogRef = this.matDialog.open(ModalAccountSyncComponent, matDialogConfig);
-    }
-
   }
 
 }
