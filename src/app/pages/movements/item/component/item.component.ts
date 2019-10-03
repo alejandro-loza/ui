9692315bff118a43d0dcdcb95a5f9da714a157ee
Alan Vargas <alan.vargas@finerio.mx>
2019-10-03 13:09:28 -0500

    hide inactive banks and new banks images

--- src/app/pages/movements/item/component/item.component.ts
+++ src/app/pages/movements/item/component/item.component.ts
@@ -1,87 +1,84 @@
-import {
-  Component,
-  OnInit,
-  Input, EventEmitter, Output,
-} from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 import { AccountService } from '@services/account/account.service';
-import {MovementsService} from '@services/movements/movements.service';
-import {StatefulMovementsService} from '@services/stateful/movements/stateful-movements.service';
-import {EditMovementListService} from '@services/movements/edit-list/edit-movement-list.service';
-import {ToastService} from '@services/toast/toast.service';
+import { MovementsService } from '@services/movements/movements.service';
+import { StatefulMovementsService } from '@services/stateful/movements/stateful-movements.service';
+import { EditMovementListService } from '@services/movements/edit-list/edit-movement-list.service';
+import { ToastService } from '@services/toast/toast.service';
 
 import { Movement } from '@interfaces/movement.interface';
 
-import {Subscription} from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-item',
-  templateUrl: './item.component.html',
-  styleUrls: [ './item.component.css' ]
+	selector: 'app-item',
+	templateUrl: './item.component.html',
+	styleUrls: [ './item.component.css' ]
 })
-
 export class ItemComponent implements OnInit {
-  @Input() movement: Movement;
-  @Output() movementChange: EventEmitter<Movement>;
-  @Output() editMovement: EventEmitter<void>;
+	@Input() movement: Movement;
+	@Output() movementChange: EventEmitter<Movement>;
+	@Output() editMovement: EventEmitter<void>;
 
-  switcherSubscription: Subscription;
+	switcherSubscription: Subscription;
 
-  traditionalImgSrc: string;
-  manualAccountImgSrc: string;
-  accountWithOutDefaults: string;
+	traditionalImgSrc: string;
+	manualAccountImgSrc: string;
+	accountWithOutDefaults: string;
 
-  constructor(
-    private accountService: AccountService,
-    private movementsService: MovementsService,
-    private statefulMovementsService: StatefulMovementsService,
-    private editMovementListService: EditMovementListService,
-    private toastService: ToastService,
-  ) {
-    this.editMovement = new EventEmitter();
-    this.movementChange = new EventEmitter();
-  }
+	constructor(
+		private accountService: AccountService,
+		private movementsService: MovementsService,
+		private statefulMovementsService: StatefulMovementsService,
+		private editMovementListService: EditMovementListService,
+		private toastService: ToastService
+	) {
+		this.editMovement = new EventEmitter();
+		this.movementChange = new EventEmitter();
+	}
 
-  ngOnInit() {
-    this.accountWithOutDefaults = this.accountService.getManualAccountNatureWithOutDefaults( this.movement.account.type );
-    this.manualAccountImgSrc = `assets/media/img/manual_account/${this.accountWithOutDefaults}.svg`;
-    this.traditionalImgSrc = `https://cdn.finerio.mx/banks/${this.movement.account.institution.code}_shield.png`;
-  }
+	ngOnInit() {
+		this.accountWithOutDefaults = this.accountService.getManualAccountNatureWithOutDefaults(
+			this.movement.account.type
+		);
+		this.manualAccountImgSrc = `assets/media/img/manual_account/${this.accountWithOutDefaults}.svg`;
+		this.traditionalImgSrc = `assets/media/img/banks/shields/${this.movement.account.institution.code.toLocaleLowerCase()}_shield.png`;
+	}
 
-  onErrorFunc(type: string) {
-    this.accountWithOutDefaults = this.accountService.getManualAccountNatureWithOutDefaults(type);
-    this.manualAccountImgSrc = `assets/media/img/manual_account/${this.accountWithOutDefaults}.svg`;
-  }
+	onErrorFunc(type: string) {
+		this.accountWithOutDefaults = this.accountService.getManualAccountNatureWithOutDefaults(type);
+		this.manualAccountImgSrc = `assets/media/img/manual_account/${this.accountWithOutDefaults}.svg`;
+	}
 
-  updateMovement(movement: Movement) {
-    this.switcherSubscription = this.movementsService.updateMovement(movement).subscribe(
-      res => {
-        this.statefulMovementsService.setMovement = movement;
-        this.toastService.setCode = res.status;
-      },
-      (err) => {
-        this.toastService.setCode = err.status;
-        if (err.status === 401) {
-          this.toastService.toastGeneral();
-          this.updateMovement(movement);
-        }
-        if (err.status === 500) {
-          this.toastService.setMessage = '¡Ha ocurrido un error al crear tu movimiento!';
-          this.toastService.toastGeneral();
-        }
-      },
-      () => {
-        this.editMovementListService.editMovement();
-        this.toastService.setMessage = 'Se editó su movimiento exitosamente';
-        this.toastService.toastGeneral();
-        this.switcherSubscription.unsubscribe();
-      }
-    );
-  }
+	updateMovement(movement: Movement) {
+		this.switcherSubscription = this.movementsService.updateMovement(movement).subscribe(
+			(res) => {
+				this.statefulMovementsService.setMovement = movement;
+				this.toastService.setCode = res.status;
+			},
+			(err) => {
+				this.toastService.setCode = err.status;
+				if (err.status === 401) {
+					this.toastService.toastGeneral();
+					this.updateMovement(movement);
+				}
+				if (err.status === 500) {
+					this.toastService.setMessage = '¡Ha ocurrido un error al crear tu movimiento!';
+					this.toastService.toastGeneral();
+				}
+			},
+			() => {
+				this.editMovementListService.editMovement();
+				this.toastService.setMessage = 'Se editó su movimiento exitosamente';
+				this.toastService.toastGeneral();
+				this.switcherSubscription.unsubscribe();
+			}
+		);
+	}
 
-  changeDuplicated(changed: boolean) {
-    this.movement = { ...this.movement, duplicated: changed};
-    this.updateMovement(this.movement);
-    this.movementChange.next(this.movement);
-  }
+	changeDuplicated(changed: boolean) {
+		this.movement = { ...this.movement, duplicated: changed };
+		this.updateMovement(this.movement);
+		this.movementChange.next(this.movement);
+	}
 }
