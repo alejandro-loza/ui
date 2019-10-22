fec83156319bca0e593636b1a827423c13526eca
Alan Vargas <alan.vargas@finerio.mx>
2019-10-22 15:23:11 -0500

    tracking events donde

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseAnalyticsService } from '@services/firebase/firebase-analytics/firebase-analytics.service';
 import { MixpanelService } from '@services/mixpanel/mixpanel.service';
+import { FacebookService } from "@services/facebook/facebook.service";
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: [ './app.component.css' ]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-	constructor(private firebaseAnalyticsService: FirebaseAnalyticsService, private mixpanel: MixpanelService) {}
+  constructor(private firebaseAnalyticsService: FirebaseAnalyticsService,
+    private mixpanel: MixpanelService,
+    private facebookService: FacebookService) { }
 
-	ngOnInit() {
-		this.firebaseAnalyticsService.initApplication();
-		this.mixpanel.initMixpanel();
-	}
+  ngOnInit() {
+    this.firebaseAnalyticsService.initApplication();
+    this.mixpanel.initMixpanel();
+    this.facebookService.initFacebookPixel();
+  }
 }
